03835601686a3b62f334bfa07c5d6178c5528c2a
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-29 20:56:02 +0100

    Create User: the API view is now a function-based endpoint since it doesn't use any parent class attributes or functions. The logic has also been altered to now create a new team if no invite_code is found in the request, but if there is one found and it doesn't correspond to an existing team, an error response is returned.

--- goteam/urls.py
+++ goteam/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import UserCreate
+from main.views import user
 
 urlpatterns = [
-    path('user/', UserCreate.as_view()),
+    path('user/', user),
 
     path('admin/', admin.site.urls)
 ]
