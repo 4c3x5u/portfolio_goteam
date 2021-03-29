100ee38fb9048fc1c2b2d54e970a675cdcf08f47
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-29 11:56:50 +0100

    All models registered inside admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(User)
+admin.site.register(Board)
+admin.site.register(Column)
+admin.site.register(Task)
+admin.site.register(Subtask)
