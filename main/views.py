a1fc998447b888729372084f8cb950416f26446c
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-29 20:56:02 +0100

    Create User: the API view is now a function-based endpoint since it doesn't use any parent class attributes or functions. The logic has also been altered to now create a new team if no invite_code is found in the request, but if there is one found and it doesn't correspond to an existing team, an error response is returned.

--- main/views.py
+++ main/views.py
@@ -1,42 +1,33 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.exceptions import ValidationError
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.core.exceptions import ObjectDoesNotExist
 from .serializers import UserSerializer
-from .models import Team, User
+from .models import Team
 
 
-class UserCreate(CreateAPIView):
-    serializer_class = UserSerializer
-
-    def create(self, request, *args, **kwargs):
-        pw = request.data.get('password')
-        cf = request.data.get('password_confirmation')
+@api_view(['POST'])
+def user(request):
+    pw = request.data.get('password')
+    cf = request.data.get('password_confirmation')
+    if pw == cf:
         ic = request.data.get('invite_code')
-        if pw == cf:
-            team = None
-            is_admin = False
+        if ic:
             try:
                 team = Team.objects.get(invite_code=ic)
+                is_admin = False
             except Team.DoesNotExist:
-                team = Team.objects.create()
-                is_admin = True
-            finally:
-                user = {'username': request.data.get('username'),
-                        'password': request.data.get('password'),
-                        'team': team.id,
-                        'is_admin': is_admin}
-                print(f'USER: {user}')
-                serializer = UserSerializer(data=user)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(user)
-                else:
-                    is_admin and team.delete()
-                    return Response(serializer.errors )
-        return Response({'password': "confirmation doesn't match password"})
-
-
-# @api_view(['POST'])
-# def team(request):
-#     return HttpResponse(Team.objects.create())
+                return Response({'invite_code': 'invalid invite code'})
+        else:
+            team = Team.objects.create()
+            is_admin = True
+        new_user = {'username': request.data.get('username'),
+                    'password': request.data.get('password'),
+                    'team': team.id,
+                    'is_admin': is_admin}
+        serializer = UserSerializer(data=new_user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(new_user)
+        else:
+            is_admin and team.delete()
+            return Response(serializer.errors)
+    return Response({'password': "confirmation doesn't match password"})
