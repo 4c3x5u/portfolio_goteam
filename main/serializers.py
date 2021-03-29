f3b74492cdc23967bec05faa858ff158d429cbb8
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-29 19:04:17 +0100

    Create User: user's team is determined by the invite_code passed inside the request. If no team responds to an invite code, a new team is create. To think about it, I'm not sure whether this is what I'd like to happen. I might change this later on.

--- main/serializers.py
+++ main/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
-from main.models import User
+from main.models import Team, User
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=255,
                                      style={'input_type': 'password'})
-
     class Meta:
         model = User
         fields = ('username', 'password', 'team')
