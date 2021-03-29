bc90f8ca1e3e93e526b3d9e29c20ede249c2a1aa
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-29 16:10:15 +0100

    Basic create user endpoint implemented.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.exceptions import ValidationError
+from .serializers import UserSerializer
+from .models import User, Team
 
-# Create your views here.
+
+class UserCreate(CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        pw = request.data.get('password')
+        cf = request.data.get('password_confirmation')
+        if pw == cf:
+            serializer = UserSerializer(data=request.data, many=False)
+            serializer.is_valid()
+            return super().create(request, *args, **kwargs)
+        else:
+            return ValidationError({
+                'password_confirmation': 'Password confirmation must match the'
+                                         'password'
+            })
+
+
+# @api_view(['POST'])
+# def team(request):
+#     return HttpResponse(Team.objects.create())
