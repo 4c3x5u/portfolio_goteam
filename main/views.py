ed0edbaed525f40794e5d66938aeeb014c3307a2
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-29 19:04:17 +0100

    Create User: user's team is determined by the invite_code passed inside the request. If no team responds to an invite code, a new team is create. To think about it, I'm not sure whether this is what I'd like to happen. I might change this later on.

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
 from .serializers import UserSerializer
-from .models import Team
+from .models import Team, User
 
 
 class UserCreate(CreateAPIView):
@@ -11,19 +12,29 @@ class UserCreate(CreateAPIView):
     def create(self, request, *args, **kwargs):
         pw = request.data.get('password')
         cf = request.data.get('password_confirmation')
+        ic = request.data.get('invite_code')
         if pw == cf:
-            serializer = UserSerializer(data=request.data, many=False)
-            serializer.is_valid()
+            team = None
+            is_admin = False
             try:
-                Team.objects.get(id=request.data.get('team'))
-            except ObjectDoesNotExist:
+                team = Team.objects.get(invite_code=ic)
+            except Team.DoesNotExist:
                 team = Team.objects.create()
-                request.data.update({'team': team.id})
-            return super().create(request, *args, **kwargs)
-        return ValidationError({
-            'password_confirmation': 'Password confirmation must match the'
-                                     'password'
-        })
+                is_admin = True
+            finally:
+                user = {'username': request.data.get('username'),
+                        'password': request.data.get('password'),
+                        'team': team.id,
+                        'is_admin': is_admin}
+                print(f'USER: {user}')
+                serializer = UserSerializer(data=user)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(user)
+                else:
+                    is_admin and team.delete()
+                    return Response(serializer.errors )
+        return Response({'password': "confirmation doesn't match password"})
 
 
 # @api_view(['POST'])
