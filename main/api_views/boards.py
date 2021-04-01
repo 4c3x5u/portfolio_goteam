4eb20513d53b6f2db00a5b3f88338e814fb33e8c
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-01 05:35:41 +0100

    List and Create board serializers unified into a single Board serializer. Their previously seperated view functions are also unified into a single boards function that accepts both GET and POST requests. All the pre-existing tests are fixed but more cases need to be added.

--- main/api_views/boards.py
+++ main/api_views/boards.py
@@ -1,29 +1,42 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from main.serializers.createboardserializer import CreateBoardSerializer
-from main.serializers.listboardsserializer import ListBoardsSerializer
-from main.models import Board
+from rest_framework.exceptions import ErrorDetail
+from ..serializers.boardserializer import BoardSerializer
+from ..models import Board
 
 
 @api_view(['POST', 'GET'])
-def board(request):
+def boards(request):
     if request.method == 'POST':
-        serializer = CreateBoardSerializer(data=request.data)
+        serializer = BoardSerializer(data={
+            'username': request.data.get('username'),
+            'team_id': request.data.get('team_id')
+        })
         if serializer.is_valid():
-            new_board = serializer.save()
+            board_response = serializer.save()
             return Response({
                 'msg': 'Board created successfuly',
-                'board_id': new_board.id,
-                'team_id': new_board.team.id
+                'board_id': board_response.id,
+                'team_id': board_response.team.id
             }, 201)
         return Response(serializer.errors, 400)
 
     if request.method == 'GET':
-        serializer = ListBoardsSerializer(
-            data={'team_id': request.query_params.get('team_id')}
+        team_id = request.query_params.get('team_id')
+        boards = Board.objects.filter(team=team_id)
+        if not boards:
+            return Response({
+                'team_id': ErrorDetail(
+                    string='No boards found for this team ID.',
+                    code='not_found'
+                )
+            })
+        serializer = BoardSerializer(
+            data=list(map(lambda b: b.__dict__, boards)),
+            many=True
         )
         if serializer.is_valid():
             return Response({
-                'boards': serializer.get_list(serializer.validated_data['team_id']),
+                'boards': serializer.validated_data
             }, 200)
         return Response(serializer.errors, 400)
--- main/tests/test_createboard.py
+++ main/tests/test_createboard.py
@@ -6,48 +6,49 @@ from ..models import Board, Team, User
 class CreateBoardTests(APITestCase):
     def setUp(self):
         self.url = '/boards/'
-        self.team = Team.objects.create()
 
     def test_success(self):
         initial_count = Board.objects.count()
-        User.objects.create(username='foooo',
-                            password='barbarbar',
-                            is_admin=True,
-                            team=self.team)
-        response = self.client.post(self.url, {'username': 'foooo'})
-        board = Board.objects.get(team=self.team)
+        team = Team.objects.create()
+        user = User.objects.create(username='foooo',
+                                   password='barbarbar',
+                                   is_admin=True,
+                                   team=team)
+        response = self.client.post(self.url, {'username': user.username,
+                                               'team_id': team.id})
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data, {
-            'msg': 'Board created successfuly',
-            'team_id': self.team.id,
-            'board_id': board.id
-        })
+        self.assertTrue(response.data.get('msg'))
+        self.assertEqual(response.data.get('team_id'), team.id)
+        self.assertEqual(response.data.get('msg'), 'Board created successfuly')
         self.assertEqual(Board.objects.count(), initial_count + 1)
-        self.assertEqual(board.team, self.team)
 
     def test_username_invalid(self):
         initial_count = Board.objects.count()
-        response = self.client.post(self.url, {'username': 'some_username'})
+        team = Team.objects.create()
+        response = self.client.post(self.url, {'username': 'some_username',
+                                               'team_id': team.id})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {
-            'username': [
-                ErrorDetail(string='Invalid username.', code='invalid')
-            ]
+            'username': ErrorDetail(string='Invalid username.', code='invalid')
         })
         self.assertEqual(Board.objects.count(), initial_count)
 
     def test_user_not_admin(self):
         initial_count = Board.objects.count()
-        User.objects.create(username='foooo',
-                            password='barbarbar',
-                            is_admin=False,
-                            team=self.team)
-        response = self.client.post(self.url, {'username': 'foooo'})
+        team = Team.objects.create()
+        user = User.objects.create(username='foooo',
+                                   password='barbarbar',
+                                   is_admin=False,
+                                   team=team)
+        response = self.client.post(self.url, {
+            'username': user.username,
+            'team_id': team.id
+        })
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {
-            'non_field_errors': [
-                ErrorDetail(string='Only the team admin can create a board.',
-                            code='not_authorized')
-            ]
+            'is_admin': ErrorDetail(
+                string='Only the team admin can create a board.',
+                code='not_authorized'
+            )
         })
         self.assertEqual(Board.objects.count(), initial_count)
