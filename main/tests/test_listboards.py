d93eb9f26a631c9faa29adfe0fd1c0b373ce452e
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-01 06:14:06 +0100

    Success test case added to the list boards tests.

--- main/tests/test_listboards.py
+++ main/tests/test_listboards.py
@@ -5,15 +5,18 @@ from ..models import Board, Team, User
 
 class ListBoardsTests(APITestCase):
     def setUp(self):
-        team = Team.objects.create()
+        self.team = Team.objects.create()
+        self.boards = []
         for _ in range(0, 3):
-            Board.objects.create(team_id=team.id)
+            self.boards.append(Board.objects.create(team_id=self.team.id))
         self.base_url = '/boards/'
-        self.team_id = team.id
+        self.team_id = self.team.id
 
     def test_success(self):
         response = self.client.get(f'{self.base_url}?team_id={self.team_id}')
-        print(f'§{response.data}')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.data.get('boards'))
-
+        boards = response.data.get('boards')
+        self.assertTrue(boards)
+        self.assertTrue(boards.count, 3)
+        for board in boards:
+            self.assertEqual(board.get('team_id'), self.team.id)
