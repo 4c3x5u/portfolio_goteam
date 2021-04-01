8b2adde7eecd942f4af6cdc70c793409bbf3303e
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-01 23:23:18 +0100

    Title max length test succeeds.

--- main/serializers/ser_task.py
+++ main/serializers/ser_task.py
@@ -3,12 +3,10 @@ from ..models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(
-        max_length=50,
-        error_messages={
-            'blank': 'Title cannot be empty.'
-        }
-    )
+    title = serializers.CharField( max_length=50, error_messages={
+        'blank': 'Title cannot be empty.',
+        'max_length': 'Title cannot be longer than 50 characters.'
+    })
 
     class Meta:
         model = Task
