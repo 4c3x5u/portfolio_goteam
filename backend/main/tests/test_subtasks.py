318acdc16d90deb54fcfb9ce63647ac16c331437
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-02 21:27:37 +0100

    Update done case is added to the subtask tests.

--- backend/main/tests/test_subtasks.py
+++ backend/main/tests/test_subtasks.py
@@ -2,7 +2,7 @@ from rest_framework.test import APITestCase
 from ..models import Subtask, Task, Column, Board, Team
 
 
-class UpdateSubtask(APITestCase):
+class SubtaskTests(APITestCase):
     def setUp(self):
         self.url = '/subtasks/'
         self.subtask = Subtask.objects.create(
@@ -20,16 +20,20 @@ class UpdateSubtask(APITestCase):
             )
         )
 
-    def test_update_title_success(self):
-        request = {
-            'id': self.subtask.id,
-            'data': {'title': 'New Task Title'}
-        }
+    def help_test_success(self, request):
         response = self.client.patch(self.url, request, format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {
-            'msg': 'Subtask update successful.',
-            'id': self.subtask.id
-        })
+        self.assertEqual(response.data, {'msg': 'Subtask update successful.',
+                                         'id': self.subtask.id})
+        return Subtask.objects.get(id=self.subtask.id)
+
+    def test_update_title_success(self):
+        request = {'id': self.subtask.id, 'data': {'title': 'New Task Title'}}
+        subtask = self.help_test_success(request)
+        self.assertEqual(subtask.title, request.get('data').get('title'))
+
+    def test_update_done_success(self):
+        request = {'id': self.subtask.id, 'data': {'done': True}}
         subtask = Subtask.objects.get(id=self.subtask.id)
-        self.assertEqual(subtask.title, request['data']['title'])
+        self.assertEqual(subtask.done, request.get('data').get('done'))
+
