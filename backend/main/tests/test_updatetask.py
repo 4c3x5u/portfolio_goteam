2e0b7c39c41f42e790621b4463c27208ece14c2a
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-02 22:43:34 +0100

    Title success case is added to the update task tests.

--- backend/main/tests/test_updatetask.py
+++ backend/main/tests/test_updatetask.py
@@ -6,7 +6,7 @@ class UpdateTaskTests(APITestCase):
     def setUp(self):
         self.url = '/tasks/'
         self.task = Task.objects.create(
-            title="Some Subtask Title",
+            title="Task Title",
             order=0,
             column=Column.objects.create(
                 order=0,
@@ -15,3 +15,14 @@ class UpdateTaskTests(APITestCase):
                 )
             )
         )
+
+    def test_title_success(self):
+        request = {'id': self.task.id, 'data': {'title': 'New Title'}}
+        response = self.client.patch(self.url, request, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {
+            'msg': 'Task update successful.',
+            'id': self.task.id
+        })
+        self.assertEqual(Task.objects.get(id=self.task.id).title,
+                         request.get('data').get('title'))
