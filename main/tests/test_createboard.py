01c87c83d98e24220a1b351576a67074a1af2e62
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-02 13:07:27 +0100

    Team ID blank test added to the create board tests. Creation of member and admin users moved into the setUp function since now they're being used for multiple tests.

--- main/tests/test_createboard.py
+++ main/tests/test_createboard.py
@@ -6,29 +6,32 @@ from ..models import Board, Team, User, Column
 class CreateBoardTests(APITestCase):
     def setUp(self):
         self.url = '/boards/'
+        self.team = Team.objects.create()
+        self.admin = User.objects.create(username='teamadmin',
+                                         password='adminpassword',
+                                         is_admin=True,
+                                         team=self.team)
+        self.member = User.objects.create(username='teammember',
+                                          password='memberpassword',
+                                          is_admin=False,
+                                          team=self.team)
 
     def test_success(self):
         initial_count = Board.objects.count()
-        team = Team.objects.create()
-        user = User.objects.create(username='foooo',
-                                   password='barbarbar',
-                                   is_admin=True,
-                                   team=team)
-        response = self.client.post(self.url, {'username': user.username,
-                                               'team_id': team.id})
+        response = self.client.post(self.url, {'username': self.admin.username,
+                                               'team_id': self.team.id})
         self.assertEqual(response.status_code, 201)
         board_id = response.data.get('board_id')
         self.assertTrue(board_id)
-        self.assertEqual(response.data.get('team_id'), team.id)
+        self.assertEqual(response.data.get('team_id'), self.team.id)
         self.assertEqual(Board.objects.count(), initial_count + 1)
         columns = Column.objects.filter(board=board_id)
         self.assertEqual(len(columns), 4)
 
     def test_username_blank(self):
         initial_count = Board.objects.count()
-        team = Team.objects.create()
         response = self.client.post(self.url, {'username': '',
-                                               'team_id': team.id})
+                                               'team_id': self.team.id})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {
             'username': ErrorDetail(string='Username cannot be empty.',
@@ -38,9 +41,8 @@ class CreateBoardTests(APITestCase):
 
     def test_username_invalid(self):
         initial_count = Board.objects.count()
-        team = Team.objects.create()
-        response = self.client.post(self.url, {'username': 'some_username',
-                                               'team_id': team.id})
+        response = self.client.post(self.url, {'username': 'invalidio',
+                                               'team_id': self.team.id})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {
             'username': ErrorDetail(string='Invalid username.', code='invalid')
@@ -49,14 +51,9 @@ class CreateBoardTests(APITestCase):
 
     def test_user_not_admin(self):
         initial_count = Board.objects.count()
-        team = Team.objects.create()
-        user = User.objects.create(username='foooo',
-                                   password='barbarbar',
-                                   is_admin=False,
-                                   team=team)
         response = self.client.post(self.url, {
-            'username': user.username,
-            'team_id': team.id
+            'username': self.member.username,
+            'team_id': self.team.id
         })
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {
@@ -64,5 +61,16 @@ class CreateBoardTests(APITestCase):
                 string='Only the team admin can create a board.',
                 code='not_authorized'
             )
+        })
+        self.assertEqual(Board.objects.count(), initial_count)
+
+    def test_team_id_blank(self):
+        initial_count = Board.objects.count()
+        response = self.client.post(self.url, {'username': self.admin.username,
+                                               'team_id': ''})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, {
+            'team_id': ErrorDetail(string='Team ID cannot be empty.',
+                                   code='blank')
         })
         self.assertEqual(Board.objects.count(), initial_count)
