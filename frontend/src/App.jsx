b4167354e36f538fd76db7010def5184a25568b6
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-03 23:25:06 +0100

    'react-router-dom' added to the project. The app component wraps its content in a Router, and a route for the register page added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from './components/register/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.sass';
 
 const App = () => (
-  <div className="App">
-    <Register />
-  </div>
+  <Router className="App">
+    <Switch>
+      <Route exact path="/" component={Register} />
+    </Switch>
+  </Router>
 );
 
 export default App;
