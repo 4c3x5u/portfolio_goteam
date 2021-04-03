2f4071a3f7e4c5d7bf54c991885fb4f80eecabac
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-03 05:00:30 +0100

    Register form labels and input boxes adjusted.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Register from './components/register/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.sass';
 
 const App = () => (
   <div className="App">
