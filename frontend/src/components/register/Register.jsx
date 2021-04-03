72bd5c466e60124eb9325798548cf50b1e684ceb
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-03 23:17:39 +0100

    Login section added to the register form.

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -9,52 +9,59 @@ const Register = () => {
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const handleSubmit = () => 1; // TODO: implement
   return (
-    <div id="FormWrapper">
-      <Form id="RegisterForm" onSubmit={handleSubmit}>
-        <div id="RegisterTitleWrapper">
-          <img id="RegisterTitle" alt="logo" src={logo} />
+    <div id="Register">
+      <Form className="Form" onSubmit={handleSubmit}>
+        <div className="TitleWrapper">
+          <img className="Title" alt="logo" src={logo} />
         </div>
 
-        <Form.Group className="RegisterFormGroup">
-          <Form.Label className="RegisterFormLabel">
+        <Form.Group className="Group">
+          <Form.Label className="Label">
             USERNAME
           </Form.Label>
           <Form.Control
-            className="RegisterInput"
+            className="Input"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </Form.Group>
 
-        <Form.Group className="RegisterFormGroup">
-          <Form.Label className="RegisterFormLabel">
+        <Form.Group className="Group">
+          <Form.Label className="Label">
             PASSWORD
           </Form.Label>
           <Form.Control
-            className="RegisterInput"
+            className="Input"
             type="password"
             value={passwordConfirmation}
             onChange={(e) => setPasswordConfirmation(e.target.value)}
           />
         </Form.Group>
 
-        <Form.Group className="RegisterFormGroup">
-          <Form.Label className="RegisterFormLabel">
+        <Form.Group className="Group">
+          <Form.Label className="Label">
             PASSWORD CONFIRMATION
           </Form.Label>
           <Form.Control
-            className="RegisterInput"
+            className="Input"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
 
-        <div id="ButtonWrapper">
-          <Button id="SubmitButton" value="GO!" type="submit">
+        <div className="ButtonWrapper">
+          <Button className="Button" value="GO!" type="submit">
             GO!
           </Button>
+        </div>
+
+        <div className="Login">
+          <p>Already have an account?</p>
+          <p>
+            <a href="/login">Login here.</a>
+          </p>
         </div>
       </Form>
     </div>
