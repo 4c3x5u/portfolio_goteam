2bb8618c7f958a01837ac47c5cc3626625cce969
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-04 21:09:04 +0100

    A basic and ugly team controls dropdown created.

--- frontend/src/components/Home/subcomponents/ControlButton.jsx
+++ frontend/src/components/Home/subcomponents/ControlButton.jsx
@@ -2,23 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
-const ControlButton = ({ name, action, icon }) => (
-  <Col xs={4} className="ControlCol">
+const ControlButton = ({
+  name, isToggled, setIsToggled, icon,
+}) => (
+  <Col xs={4} className="ControlButtonWrapper">
     <Button
       className="Button"
-      onClick={action}
+      onClick={setIsToggled}
       aria-label={`${name} button`}
     >
       <FontAwesomeIcon icon={icon} />
       {name.toUpperCase()}
     </Button>
+    {isToggled && (
+      <div className="TeamControls">
+        <ul>
+          <li>Some Member</li>
+          <li>Some Other Member</li>
+          <Button>
+            <FontAwesomeIcon icon={faPlusCircle} />
+          </Button>
+        </ul>
+      </div>
+    )}
+
   </Col>
 );
 
 ControlButton.propTypes = {
   name: PropTypes.string.isRequired,
-  action: PropTypes.func.isRequired,
+  isToggled: PropTypes.bool.isRequired,
+  setIsToggled: PropTypes.func.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   icon: PropTypes.object.isRequired,
 };
--- frontend/src/components/Home/subcomponents/Header.jsx
+++ frontend/src/components/Home/subcomponents/Header.jsx
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import {
-  faChalkboardTeacher,
-  faQuestionCircle,
-  faUsers,
+  faChalkboardTeacher, faQuestionCircle, faUsers,
 } from '@fortawesome/free-solid-svg-icons';
 import ControlButton from './ControlButton';
 import logo from '../../../assets/homeHeader.svg';
 
-const Header = () => (
-  <div className="Header">
-    <div className="Logo">
-      <img alt="logo" src={logo} />
-    </div>
-    <div className="ControlBar">
-      <Container>
-        <Row className="Controls">
-          <ControlButton
-            name="team"
-            action={() => console.log('team button clicked')}
-            icon={faUsers}
-          />
+const Header = () => {
+  const [teamCtrlIsToggled, setTeamCtrlIsToggled] = useState(false);
 
-          <ControlButton
-            name="boards"
-            action={() => console.log('boards button clicked')}
-            icon={faChalkboardTeacher}
-          />
+  return (
+    <div className="Header">
+      <div className="Logo">
+        <img alt="logo" src={logo} />
+      </div>
+      <div className="ControlBar">
+        <Container>
+          <Row className="Controls">
+            <ControlButton
+              name="team"
+              setIsToggled={() => setTeamCtrlIsToggled(!teamCtrlIsToggled)}
+              isToggled={teamCtrlIsToggled}
+              icon={faUsers}
+            />
 
-          <ControlButton
-            name="help"
-            action={() => console.log('boards button clicked')}
-            icon={faQuestionCircle}
-          />
-        </Row>
-      </Container>
+            <ControlButton
+              name="boards"
+              action={() => console.log('boards button clicked')}
+              icon={faChalkboardTeacher}
+            />
+
+            <ControlButton
+              name="help"
+              action={() => console.log('boards button clicked')}
+              icon={faQuestionCircle}
+            />
+          </Row>
+        </Container>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
