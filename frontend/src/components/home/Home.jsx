150a9d6b90363a293a3522e535deb2c8175e98ff
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-04 13:17:04 +0100

    All column headers created.

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import {
   faUsers,
   faChalkboardTeacher,
@@ -9,10 +9,14 @@ import ControlButton from './ControlButton';
 
 import './home.sass';
 import logo from '../../assets/homeheader.svg';
+import inbox from '../../assets/inbox.svg';
+import ready from '../../assets/ready.svg';
+import go from '../../assets/go.svg';
+import done from '../../assets/done.svg';
 
 const Home = () => (
   <div id="Home">
-    <div className="Header">
+    <div className="Logo">
       <img alt="logo" src={logo} />
     </div>
     <div className="ControlBar">
@@ -38,6 +42,44 @@ const Home = () => (
         </Row>
       </Container>
     </div>
+
+    <Row className="ColumnsRow">
+      <Col className="Col" xs={3}>
+        <div className="Column InboxColumn">
+          <div className="Header">
+            <img src={inbox} alt="inbox column header" />
+          </div>
+          <div className="Body" />
+        </div>
+      </Col>
+
+      <Col className="Col" xs={3}>
+        <div className="Column ReadyColumn">
+          <div className="Header">
+            <img src={ready} alt="ready column header" />
+          </div>
+          <div className="Body" />
+        </div>
+      </Col>
+
+      <Col className="Col" xs={3}>
+        <div className="Column GoColumn">
+          <div className="Header">
+            <img src={go} alt="go column header" />
+          </div>
+          <div className="Body" />
+        </div>
+      </Col>
+
+      <Col className="Col" xs={3}>
+        <div className="Column DoneColumn">
+          <div className="Header">
+            <img src={done} alt="done column header" />
+          </div>
+          <div className="Body" />
+        </div>
+      </Col>
+    </Row>
   </div>
 );
 
