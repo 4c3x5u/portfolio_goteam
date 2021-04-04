e9b6e6b3917f94ad053b17dc38bc6dcf2b770591
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-04 01:15:34 +0100

    Login page implemented, using pretty much the same code as the register component with a couple of small changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Register from './components/register/Register';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.sass';
 
 const App = () => (
   <Router className="App">
     <Switch>
-      <Route exact path="/" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <Route path="/" component={Register} />
     </Switch>
   </Router>
 );
