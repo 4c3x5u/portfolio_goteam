119217afc0ad73ba76806d024c33694f0ed85c08
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-04 10:36:57 +0100

    Control bar added to the home page with three buttons: team, boards, and help.

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 import './home.sass';
 import logo from '../../assets/homeheader.svg';
 
@@ -6,6 +7,39 @@ const Home = () => (
   <div id="Home">
     <div className="Header">
       <img alt="logo" src={logo} />
+    </div>
+    <div className="ControlBar">
+      <Container>
+        <ul>
+          <li>
+            <Button
+              className="Button"
+              onClick={() => console.log('team button clicked')}
+              aria-label="team controls"
+            >
+              TEAM
+            </Button>
+          </li>
+          <li>
+            <Button
+              className="Button"
+              onClick={() => console.log('boards button clicked')}
+              aria-label="boards controls"
+            >
+              BOARDS
+            </Button>
+          </li>
+          <li>
+            <Button
+              className="Button"
+              onClick={() => console.log('help button clicked')}
+              aria-label="help button"
+            >
+              HELP
+            </Button>
+          </li>
+        </ul>
+      </Container>
     </div>
   </div>
 );
