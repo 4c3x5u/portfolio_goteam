d6f9d906e8deb64259ab36c1f7aa42b63cbb5bed
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-05 13:32:02 +0100

    A very basic, static Task with only a title created.

--- frontend/src/components/Home/Body/Body.jsx
+++ frontend/src/components/Home/Body/Body.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Col, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
-import { capFirstLetterOf } from '../../../misc/util';
+import Column from './Column/Column';
 
 import './body.sass';
-
-const Column = ({ name }) => (
-  <Col className="Col" xs={3}>
-    <div className={`Column ${capFirstLetterOf(name)}Column`}>
-      <div className="Header">{name.toUpperCase()}</div>
-      <div className="Body" />
-    </div>
-  </Col>
-);
-
-Column.propTypes = { name: PropTypes.string.isRequired };
 
 const Body = () => (
   <Row id="Body">
---
+++ frontend/src/components/Home/Body/Column/Task/Task.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './task.sass';
+
+const Task = () => (
+  <div className="Task">
+    <h1>Do Something</h1>
+  </div>
+);
+
+export default Task;
