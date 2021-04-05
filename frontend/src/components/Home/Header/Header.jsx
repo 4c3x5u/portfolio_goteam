7ef8d7ecbd337065e9267928efea20e0cd20e34a
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-05 23:06:46 +0100

    Active window state moved up a level into the Home view to accommodate for the create windows.

--- frontend/src/components/Home/Header/Header.jsx
+++ frontend/src/components/Home/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Row } from 'react-bootstrap';
 import {
   faChalkboardTeacher, faUsers,
@@ -6,58 +7,48 @@ import {
 
 import ControlMenu from './ControlMenu/ControlMenu';
 import HelpModal from './HelpModal/HelpModal';
+import { windowEnum } from '../windowEnum';
 
 import logo from '../../../assets/homeHeader.svg';
 import './header.sass';
 
-const Header = () => {
-  const windowEnum = {
-    NONE: 0, TEAM: 1, BOARDS: 2, MODAL: 3,
-  };
-
-  const [activeWindow, setActiveWindow] = useState(windowEnum.NONE);
-
-  const handleActivate = (window) => () => {
-    switch (window) {
-      case activeWindow: setActiveWindow(windowEnum.NONE); break;
-      case windowEnum.TEAM: setActiveWindow(windowEnum.TEAM); break;
-      case windowEnum.BOARDS: setActiveWindow(windowEnum.BOARDS); break;
-      case windowEnum.MODAL: setActiveWindow(windowEnum.MODAL); break;
-      default: setActiveWindow(windowEnum.NONE); break;
-    }
-  };
-
-  return (
-    <div id="Header">
-      <div className="Logo">
-        <img alt="logo" src={logo} />
-      </div>
-      <div className="ControlsWrapper">
-        <Container>
-          <Row className="ControlsRow">
-            <ControlMenu
-              name="team"
-              isActive={activeWindow === windowEnum.TEAM}
-              activate={handleActivate(windowEnum.TEAM)}
-              icon={faUsers}
-            />
-
-            <ControlMenu
-              name="boards"
-              isActive={activeWindow === windowEnum.BOARDS}
-              activate={handleActivate(windowEnum.BOARDS)}
-              icon={faChalkboardTeacher}
-            />
-
-            <HelpModal
-              isActive={activeWindow === windowEnum.MODAL}
-              activate={handleActivate(windowEnum.MODAL)}
-            />
-          </Row>
-        </Container>
-      </div>
+const Header = ({ activeWindow, handleActivate }) => (
+  <div id="Header">
+    <div className="Logo">
+      <img alt="logo" src={logo} />
     </div>
-  );
+    <div className="ControlsWrapper">
+      <Container>
+        <Row className="ControlsRow">
+          <ControlMenu
+            name="team"
+            isActive={activeWindow === windowEnum.TEAM}
+            activate={handleActivate(windowEnum.TEAM)}
+            create={handleActivate(windowEnum.INVITE_MEMBER)}
+            icon={faUsers}
+          />
+
+          <ControlMenu
+            name="boards"
+            isActive={activeWindow === windowEnum.BOARDS}
+            activate={handleActivate(windowEnum.BOARDS)}
+            create={handleActivate(windowEnum.CREATE_BOARD)}
+            icon={faChalkboardTeacher}
+          />
+
+          <HelpModal
+            isActive={activeWindow === windowEnum.MODAL}
+            activate={handleActivate(windowEnum.MODAL)}
+          />
+        </Row>
+      </Container>
+    </div>
+  </div>
+);
+
+Header.propTypes = {
+  activeWindow: PropTypes.number.isRequired,
+  handleActivate: PropTypes.func.isRequired,
 };
 
 export default Header;
