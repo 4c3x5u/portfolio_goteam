02e45a2fb75250a73fd6f8a9f0fb0eac5c2395f5
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-08 11:47:05 +0100

    EditTask and EditSubtask components added, currently being triggered by a click on the any given task – soon to be changed.

---
+++ frontend/src/components/Home/Board/Column/Task/EditTask/EditSubtasks/EditSubtasks.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Form } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import './editsubtasks.sass';
+
+const EditSubtasks = ({ subtasks, setSubtasks }) => (
+  <Form.Group className="EditSubtasks">
+    <Form.Label className="Label">
+      SUBTASKS
+    </Form.Label>
+
+    {subtasks.list.map((subtask) => (
+      <div className="ControlWrapper">
+        <Form.Control
+          key={subtask.id}
+          className="Input"
+          type="text"
+          value={subtask.title}
+          onChange={() => console.log('NOT ALLOWED')}
+        />
+
+        <Button
+          className="Remove"
+          onClick={() => {
+            setSubtasks({
+              value: '',
+              list: subtasks.list.filter((st) => st.id !== subtask.id),
+            });
+          }}
+          type="button"
+        >
+          <FontAwesomeIcon className="Icon" icon={faTimes} />
+        </Button>
+      </div>
+    ))}
+
+    <div className="ControlWrapper">
+      <Form.Control
+        className="Input"
+        type="text"
+        value={subtasks.value}
+        onChange={(e) => setSubtasks({
+          ...subtasks,
+          value: e.target.value,
+        })}
+      />
+
+      <Button
+        className="Add"
+        onClick={() => setSubtasks({
+          value: '',
+          list: [...subtasks.list, {
+            id: null,
+            title: subtasks.value,
+            order: subtasks.list.length,
+            done: false,
+          }],
+        })}
+        type="button"
+      >
+        <FontAwesomeIcon className="Icon" icon={faPlus} />
+      </Button>
+    </div>
+  </Form.Group>
+);
+
+EditSubtasks.propTypes = {
+  subtasks: PropTypes.objectOf({
+    value: PropTypes.string.isRequired,
+    list: PropTypes.arrayOf(
+      PropTypes.string.isRequired,
+    ).isRequired,
+  }).isRequired,
+  setSubtasks: PropTypes.func.isRequired,
+};
+
+export default EditSubtasks;
---
+++ frontend/src/components/Home/Board/Column/Task/EditTask/EditTask.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable
+jsx-a11y/click-events-have-key-events,
+jsx-a11y/no-static-element-interactions */
+
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Form } from 'react-bootstrap';
+
+import FormGroup from '../../../../../_shared/FormGroup/FormGroup';
+import EditSubtasks from './EditSubtasks/EditSubtasks';
+import { inputType } from '../../../../../../misc/enums';
+
+import logo from '../../../../../../assets/createtask.svg';
+import './edittask.sass';
+
+const EditTask = ({
+  id, title, description, subtasks, toggleOff,
+}) => {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const [newSubtasks, setNewSubtasks] = useState({
+    value: '',
+    list: [],
+  });
+
+  useEffect(() => (
+    // TODO: use task id to find and populate subtasks.
+    setNewSubtasks({
+      value: '',
+      list: subtasks,
+    })
+  ), []);
+
+  const handleSubmit = () => (
+    // TODO: Set title, description, and subtasks as needed, submit to API
+    console.log(`Task ID: ${id}`)
+  );
+
+  return (
+    <div className="EditTask" onClick={toggleOff}>
+      <Form
+        className="Form"
+        onSubmit={handleSubmit}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="HeaderWrapper">
+          <img className="Header" alt="logo" src={logo} />
+        </div>
+
+        <FormGroup
+          type={inputType.TEXT}
+          label="title"
+          value={newTitle}
+          setValue={setNewTitle}
+        />
+
+        <FormGroup
+          type={inputType.TEXTAREA}
+          label="description"
+          value={newDescription}
+          setValue={setNewDescription}
+        />
+
+        <EditSubtasks subtasks={newSubtasks} setSubtasks={setNewSubtasks} />
+
+        <div className="ButtonWrapper">
+          <Button className="Button" type="submit" aria-label="submit">
+            GO!
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+EditTask.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  subtasks: PropTypes.arrayOf({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    order: PropTypes.number.isRequired,
+    done: PropTypes.bool.isRequired,
+  }).isRequired,
+  toggleOff: PropTypes.func.isRequired,
+};
+
+export default EditTask;
