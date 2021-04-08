f6dda96ef14e295edbdc03e9d9fc82c0766d4d34
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-08 11:47:05 +0100

    EditTask and EditSubtask components added, currently being triggered by a click on the any given task – soon to be changed.

--- frontend/src/components/Home/Board/Column/Task/Task.jsx
+++ frontend/src/components/Home/Board/Column/Task/Task.jsx
@@ -1,17 +1,22 @@
+/* eslint-disable
+jsx-a11y/click-events-have-key-events,
+jsx-a11y/no-static-element-interactions */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 
 import Subtask from './Subtask/Subtask';
+import EditTask from './EditTask/EditTask';
 
 import './task.sass';
 
 const Task = ({ id, title, description }) => {
+  const [editIsOn, setEditIsOn] = useState(false);
   const [subtasks, setSubtasks] = useState([]);
 
   // TODO: API call based on task ID
   console.log(`Task ID: ${id}`);
-  useEffect(() => (
+  useEffect(() => {
     setSubtasks([
       {
         id: 0,
@@ -25,54 +30,44 @@ const Task = ({ id, title, description }) => {
         order: 1,
         done: false,
       },
-    ])
-  ), []);
+    ]);
+  }, []);
 
   return (
-    <div className="Task">
-      <ContextMenuTrigger id={id}>
-        <div className="TaskBody">
-          <h1 className="Title">
-            {title}
-          </h1>
+    <div className="Task" onClick={setEditIsOn}>
+      <div className="TaskBody">
+        <h1 className="Title">
+          {title}
+        </h1>
 
-          <p className="Description">
-            {description}
-          </p>
+        <p className="Description">
+          {description}
+        </p>
 
-          {subtasks.length > 0 && (
-            <ul className="Subtasks">
-              {subtasks.sort((subtask) => subtask.order).map((subtask) => (
-                <Subtask
-                  id={subtask.id}
-                  title={subtask.title}
-                  done={subtask.done}
-                />
-              ))}
-            </ul>
-          )}
-        </div>
-      </ContextMenuTrigger>
+        {subtasks.length > 0 && (
+          <ul className="Subtasks">
+            {subtasks.sort((subtask) => subtask.order).map((subtask) => (
+              <Subtask
+                id={subtask.id}
+                title={subtask.title}
+                done={subtask.done}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
 
-      <ContextMenu clasName="Menu" id={id}>
-        <MenuItem>
-          <button
-            onClick={() => console.log('TODO: TOGGLE EDIT')}
-            type="button"
-          >
-            Edit Task
-          </button>
-        </MenuItem>
-
-        <MenuItem onClick={() => console.log('TODO: TOGGLE DELETE')}>
-          <button
-            onClick={() => console.log('TODO: TOGGLE EDIT')}
-            type="button"
-          >
-            Delete Task
-          </button>
-        </MenuItem>
-      </ContextMenu>
+      {
+        editIsOn && (
+          <EditTask
+            id={id}
+            title={title}
+            description={description}
+            subtasks={subtasks}
+            toggleOff={() => setEditIsOn(false)}
+          />
+        )
+      }
     </div>
   );
 };
