581760ac2c43688aaacb24bbb0f1363e90862a20
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-11 22:59:18 +0100

    Revert ""Then" and "Catch" handlers for the `verifyToken` is moved into the verifyToken function itself, which now accepts `setUser` as an argument to set the user depending on the verification outcome."
    This reverts commit 2ceb3573
    
    To think about it, a user who never had a valid token cannot in any way load the Home page, and if we delete the auth tokens and set the current user state to an unauthenticated empty user, I don't think there's a reason to validate the auth token more than once.

--- frontend/src/misc/verifyToken.js
+++ frontend/src/misc/verifyToken.js
@@ -1,20 +1,10 @@
 import axios from 'axios';
 
-const verifyToken = (setCurrentUser) => (
+const verifyToken = () => (
   axios.post(`${process.env.REACT_APP_BACKEND_URL}/verify-token/`, {
     username: sessionStorage.getItem('username'),
     token: sessionStorage.getItem('auth-token'),
-  }).then((res) => setCurrentUser({
-    username: res.data.username,
-    teamId: res.data.teamId,
-    isAdmin: res.data.isAdmin,
-    isAuthenticated: true,
-  })).catch(() => setCurrentUser({
-    username: '',
-    teamId: null,
-    isAdmin: false,
-    isAuthenticated: false,
-  }))
+  })
 );
 
 export default verifyToken;
