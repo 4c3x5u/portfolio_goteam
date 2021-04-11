7f0889a0c06c25bce8ef372d2a229de94746c6d8
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-11 22:08:13 +0100

    Validate token logic is removed from Login, Register and Home components, and moved into the App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from 'react-router-dom';
 
 import Home from './components/Home/Home';
@@ -11,25 +12,49 @@ import Register from './components/Register/Register';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.sass';
+import verifyToken from './misc/verifyToken';
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState({
     username: '',
-    teamId: '',
-    isAdmin: '',
+    teamId: null,
+    isAdmin: false,
+    isAuthenticated: false,
   });
+
+  useEffect(() => {
+    verifyToken().then((res) => setCurrentUser({
+      username: res.data.username,
+      teamId: res.data.teamId,
+      isAdmin: res.data.isAdmin,
+      isAuthenticated: true,
+    })).catch(() => setCurrentUser({
+      username: '',
+      teamId: null,
+      isAdmin: false,
+      isAuthenticated: false,
+    }));
+  }, []);
 
   return (
     <Router className="App">
       <Switch>
         <Route exact path="/">
-          <Home currentUser={currentUser} />
+          {currentUser.isAuthenticated
+            ? <Home currentUser={currentUser} />
+            : <Redirect to="/login" />}
         </Route>
+
         <Route exact path="/login">
-          <Login setCurrentUser={setCurrentUser} />
+          {!currentUser.isAuthenticated
+            ? <Login setCurrentUser={setCurrentUser} />
+            : <Redirect to="/" />}
         </Route>
+
         <Route exact path="/register">
-          <Register setCurrentUser={setCurrentUser} />
+          {!currentUser.isAuthenticated
+            ? <Register setCurrentUser={setCurrentUser} />
+            : <Redirect to="/" />}
         </Route>
       </Switch>
     </Router>
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 import FormGroup from '../_shared/FormGroup/FormGroup';
-import verifyToken from '../../misc/verifyToken';
 import validateLoginForm from './validateLoginForm';
 import inputType from '../../misc/inputType';
 
@@ -13,27 +11,10 @@ import logo from './login.svg';
 import './login.sass';
 
 const Login = ({ setCurrentUser }) => {
-  const [authenticated, setAuthenticated] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState({ username: '', password: '' });
 
-  useEffect(() => {
-    verifyToken()
-      .then((res) => {
-        setAuthenticated(true);
-        setCurrentUser({
-          username: res.data.username,
-          teamId: res.data.teamId,
-          isAdmin: res.data.isAdmin,
-        });
-      })
-      .catch(() => setAuthenticated(false));
-  }, []);
-
-  if (authenticated) { return <Redirect to="/" />; }
-
-  // TODO: implement
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -46,7 +27,12 @@ const Login = ({ setCurrentUser }) => {
       }).then((res) => {
         sessionStorage.setItem('username', res.data.username);
         sessionStorage.setItem('auth-token', res.data.token);
-        setAuthenticated(true);
+        setCurrentUser({
+          username: res.data.username,
+          teamId: res.data.teamId,
+          isAdmin: res.data.isAdmin,
+          isAuthenticated: true,
+        });
       }).catch((err) => {
         // TODO: Add toastr for server-side errors
         console.error(`SERVER-SIDE ERROR: ${JSON.stringify(err)}`);
