817dd4671dd38cc173a57d8b415921b78ffbb27b
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-12 02:42:40 +0100

    Common code for the GET and POST board endpoints moved out of the if statements and into the api view body.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,9 +139,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    # ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ]
