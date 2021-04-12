79f05178b316ae2b88454e32b8a2113442283c77
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-12 03:01:35 +0100

    Success case for the list boards tests succeeds.

--- backend/main/tests/test_listboards.py
+++ backend/main/tests/test_listboards.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework.exceptions import ErrorDetail
-from ..models import Board, Team
+from ..models import Board, Team, User
 
 
 class ListBoardsTests(APITestCase):
@@ -11,10 +11,31 @@ class ListBoardsTests(APITestCase):
             Board.objects.create(team_id=self.team.id) for _ in range(0, 3)
         ]
         self.team_id = str(self.team.id)
+        self.admin = User.objects.create(
+            username='teamadmin',
+            password=b'$2b$12$lrkDnrwXSBU.YJvdzbpAWOd9GhwHJGVYafRXTHct2gm3akPJ'
+                     b'gB5Zq',
+            is_admin=True,
+            team=self.team
+        )
+        self.member = User.objects.create(
+            username='teammember',
+            password=b'$2b$12$RonFQ1/18JiCN8yFeBaxKOsVbxLdcehlZ4e0r9gtZbARqEVU'
+                     b'HHEoK',
+            is_admin=False,
+            team=self.team
+        )
+        self.admin_token = '$2b$12$TVdxI.a.ZlOkhH1/mZQ/IOHmKxklQJWiB0n6ZSg2R' \
+                           'JJO17thjLOdy'
+        self.member_token = '$2b$12$xnIJLzpgNV12O80XsakMjezCFqwIphdBy5ziJ9Eb' \
+                            '9stnDZze19Ude'
 
     def test_success(self):
         initial_count = Board.objects.count()
-        response = self.client.get(self.base_url + self.team_id)
+        response = self.client.get(self.base_url + self.team_id,
+                                   HTTP_AUTH_USER=self.member.username,
+                                   HTTP_AUTH_TOKEN=self.member_token)
+        print(f'resposnebody: {response.data}')
         self.assertEqual(response.status_code, 200)
         boards = response.data.get('boards')
         self.assertTrue(boards)
@@ -27,7 +48,7 @@ class ListBoardsTests(APITestCase):
         initial_count = Board.objects.count()
         team = Team.objects.create()
         response = self.client.get(self.base_url + str(team.id))
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 404)
         self.assertEqual(len(response.data.get('boards')), 1)
         self.assertEqual(Board.objects.count(), initial_count + 1)
 
