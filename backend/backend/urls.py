6c00d1031476de6a109450543aa09a0045aeed60
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-13 22:31:34 +0100

    Success case for the Get Columns tests passes.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,14 +4,14 @@ from main.api.api_auth import register, login, verify_token
 from main.api.api_boards import boards
 from main.api.api_tasks import tasks
 from main.api.api_subtasks import subtasks
+from main.api.api_columns import columns
 
 urlpatterns = [
+    path('verify-token/', verify_token, name='verifytoken'),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
-    path('verify-token/', verify_token, name='verifytoken'),
     path('boards/', boards, name='board'),
+    path('columns/', columns, name='columns'),
     path('tasks/', tasks, name='tasks'),
     path('subtasks/', subtasks, name='subtasks'),
-
-    path('admin/', admin.site.urls)
 ]
