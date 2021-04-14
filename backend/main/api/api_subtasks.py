f62ac3643f6266e3650ea625afb6f32e90d9ca12
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-14 06:20:47 +0100

    GET subtasks tests written, the API endpoint created, and all the tests pass.

--- backend/main/api/api_subtasks.py
+++ backend/main/api/api_subtasks.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import ErrorDetail
-from ..models import Subtask
+from ..models import Subtask, Task
 from ..serializers.ser_subtask import SubtaskSerializer
 from ..util import authenticate, authorize
 
 
-@api_view(['PATCH'])
+@api_view(['PATCH', 'GET'])
 def subtasks(request):
     username = request.META.get('HTTP_AUTH_USER')
     token = request.META.get('HTTP_AUTH_TOKEN')
@@ -15,52 +15,96 @@ def subtasks(request):
     if authentication_response:
         return authentication_response
 
-    authorization_response = authorize(username)
-    if authorization_response:
-        return authorization_response
+    if request.method == 'GET':
+        task_id = request.query_params.get('task_id')
 
-    subtask_id = request.data.get('id')
-    if not subtask_id:
+        if not task_id:
+            return Response({
+                'task_id': ErrorDetail(string='Task ID cannot be empty.',
+                                       code='blank')
+            }, 400)
+
+        try:
+            int(task_id)
+        except ValueError:
+            return Response({
+                'task_id': ErrorDetail(string='Task ID must be a number.',
+                                       code='invalid')
+            }, 400)
+
+        try:
+            Task.objects.get(id=task_id)
+        except Task.DoesNotExist:
+            return Response({
+                'task_id': ErrorDetail(string='Task not found.',
+                                       code='not_found')
+            }, 404)
+
+        task_subtasks = Subtask.objects.filter(task_id=task_id)
+        serializer = SubtaskSerializer(task_subtasks, many=True)
         return Response({
-            'id': ErrorDetail(string='Subtask ID cannot be empty.',
-                              code='blank')
-        }, 400)
+            'subtasks': list(
+                map(
+                    lambda st: {
+                        'id': st['id'],
+                        'order': st['order'],
+                        'title': st['title'],
+                        'done': st['done']
+                    },
+                    serializer.data
+                )
+            )
+        }, 200)
 
-    data = request.data.get('data')
-    if not data:
+
+
+    if request.method == 'PATCH':
+        authorization_response = authorize(username)
+        if authorization_response:
+            return authorization_response
+
+        subtask_id = request.data.get('id')
+        if not subtask_id:
+            return Response({
+                'id': ErrorDetail(string='Subtask ID cannot be empty.',
+                                  code='blank')
+            }, 400)
+
+        data = request.data.get('data')
+        if not data:
+            return Response({
+                'data': ErrorDetail(string='Data cannot be empty.', code='blank')
+            }, 400)
+
+        if 'title' in list(data.keys()) and not data.get('title'):
+            return Response({
+                'data.title': ErrorDetail(string='Title cannot be empty.',
+                                          code='blank')
+            }, 400)
+
+        done = data.get('done')
+        if 'done' in list(data.keys()) and (done == '' or done is None):
+            return Response({
+                'data.done': ErrorDetail(string='Done cannot be empty.',
+                                         code='blank')
+            }, 400)
+
+        order = data.get('order')
+        if 'order' in list(data.keys()) and (order == '' or order is None):
+            return Response({
+                'data.order': ErrorDetail(string='Order cannot be empty.',
+                                          code='blank')
+            }, 400)
+
+        serializer = SubtaskSerializer(Subtask.objects.get(id=subtask_id),
+                                       data=data,
+                                       partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, 400)
+
+        subtask = serializer.save()
         return Response({
-            'data': ErrorDetail(string='Data cannot be empty.', code='blank')
-        }, 400)
-
-    if 'title' in list(data.keys()) and not data.get('title'):
-        return Response({
-            'data.title': ErrorDetail(string='Title cannot be empty.',
-                                      code='blank')
-        }, 400)
-
-    done = data.get('done')
-    if 'done' in list(data.keys()) and (done == '' or done is None):
-        return Response({
-            'data.done': ErrorDetail(string='Done cannot be empty.',
-                                     code='blank')
-        }, 400)
-
-    order = data.get('order')
-    if 'order' in list(data.keys()) and (order == '' or order is None):
-        return Response({
-            'data.order': ErrorDetail(string='Order cannot be empty.',
-                                      code='blank')
-        }, 400)
-
-    serializer = SubtaskSerializer(Subtask.objects.get(id=subtask_id),
-                                   data=data,
-                                   partial=True)
-    if not serializer.is_valid():
-        return Response(serializer.errors, 400)
-
-    subtask = serializer.save()
-    return Response({
-        'msg': 'Subtask update successful.',
-        'id': subtask.id
-    }, 200)
+            'msg': 'Subtask update successful.',
+            'id': subtask.id
+        }, 200)
 
---
+++ backend/main/tests/test_getsubtasks.py
@@ -0,0 +1,105 @@
+from rest_framework.test import APITestCase
+from rest_framework.exceptions import ErrorDetail
+from ..models import Subtask, Task, Column, Board, Team
+from ..util import new_member, not_authenticated_response
+
+
+class GetSubtasksTests(APITestCase):
+    endpoint = '/subtasks/?task_id='
+
+    def setUp(self):
+        team = Team.objects.create()
+        board = Board.objects.create(team=team)
+        self.column = Column.objects.create(order=0, board=board)
+        self.task = Task.objects.create(title='Some Task',
+                                        order=0,
+                                        column=self.column)
+        subtasks_raw = [
+            Subtask.objects.create(
+                title=f'Subtask #{i}',
+                order=i,
+                done=True if i == 1 else False,
+                task=self.task
+            ) for i in range(0, 3)
+        ]
+        self.subtasks = list(
+            map(
+                lambda subtask: {'id': subtask.id,
+                                 'order': subtask.order,
+                                 'title': subtask.title,
+                                 'done': subtask.done}
+                , subtasks_raw
+            )
+        )
+        self.member = new_member(team)
+
+    def test_success(self):
+        response = self.client.get(f'{self.endpoint}{self.task.id}',
+                                   HTTP_AUTH_USER=self.member['username'],
+                                   HTTP_AUTH_TOKEN=self.member['token'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get('subtasks'), self.subtasks)
+
+    def test_column_id_empty(self):
+        response = self.client.get(self.endpoint,
+                                   HTTP_AUTH_USER=self.member['username'],
+                                   HTTP_AUTH_TOKEN=self.member['token'])
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, {
+            'task_id': ErrorDetail(string='Task ID cannot be empty.',
+                                   code='blank')
+        })
+
+    def test_column_id_invalid(self):
+        response = self.client.get(f'{self.endpoint}aksjda',
+                                   HTTP_AUTH_USER=self.member['username'],
+                                   HTTP_AUTH_TOKEN=self.member['token'])
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, {
+            'task_id': ErrorDetail(string='Task ID must be a number.',
+                                   code='invalid')
+        })
+
+    def test_column_not_found(self):
+        response = self.client.get(f'{self.endpoint}1231231',
+                                   HTTP_AUTH_USER=self.member['username'],
+                                   HTTP_AUTH_TOKEN=self.member['token'])
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, {
+            'task_id': ErrorDetail(string='Task not found.',
+                                   code='not_found')
+        })
+
+    def test_auth_user_empty(self):
+        response = self.client.get(f'{self.endpoint}{self.column.id}',
+                                   HTTP_AUTH_USER='',
+                                   HTTP_AUTH_TOKEN=self.member['token'])
+        self.assertEqual(response.status_code,
+                         not_authenticated_response.status_code)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+    def test_auth_user_invalid(self):
+        response = self.client.get(f'{self.endpoint}{self.column.id}',
+                                   HTTP_AUTH_USER='invalidusername',
+                                   HTTP_AUTH_TOKEN=self.member['token'])
+        self.assertEqual(response.status_code,
+                         not_authenticated_response.status_code)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+    def test_auth_token_empty(self):
+        response = self.client.get(f'{self.endpoint}{self.column.id}',
+                                   HTTP_AUTH_USER=self.member['username'],
+                                   HTTP_AUTH_TOKEN='')
+        self.assertEqual(response.status_code,
+                         not_authenticated_response.status_code)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+    def test_auth_token_invalid(self):
+        response = self.client.get(f'{self.endpoint}{self.column.id}',
+                                   HTTP_AUTH_USER=self.member['username'],
+                                   HTTP_AUTH_TOKEN='ASDKFJ!FJ_012rjpiwajfosia')
+        self.assertEqual(response.status_code,
+                         not_authenticated_response.status_code)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+
