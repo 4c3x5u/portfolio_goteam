3acaa324ca7005852c5855ac23b4412df1df72d1
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-15 20:26:48 +0100

    Delete Task endpoint created. The success test case passes.

--- backend/main/tests/test_deletetask.py
+++ backend/main/tests/test_deletetask.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from ..models import Team, Board, Column, Task
+from ..util import new_admin
 
 
 class DeleteTaskTests(APITestCase):
@@ -9,18 +10,19 @@ class DeleteTaskTests(APITestCase):
         team = Team.objects.create()
         board = Board.objects.create(team=team)
         column = Column.objects.create(order=0, board=board)
-        self.task = Task.objects.create(
-            title='Do Something!',
-            order=0,
-            column=column
-        )
+        self.task = Task.objects.create(title='Do Something!',
+                                        order=0,
+                                        column=column)
+        self.admin = new_admin(team)
 
     def test_success(self):
         initial_count = Task.objects.count()
-        response = self.client.delete(f'{self.endpoint}{self.task.id}')
+        response = self.client.delete(f'{self.endpoint}{self.task.id}',
+                                      HTTP_AUTH_USER=self.admin['username'],
+                                      HTTP_AUTH_TOKEN=self.admin['token'])
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {
             'msg': 'Task deleted successfully.',
-            'id': self.task.id,
+            'id': str(self.task.id),
         })
         self.assertEqual(Task.objects.count(), initial_count - 1)
