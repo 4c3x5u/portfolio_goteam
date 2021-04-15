39e8aa09c6caefc6f3c6cb318659b49e3efc64db
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-15 22:14:34 +0100

    Create Board component is connected to the CREATE boards API.

--- frontend/src/components/Home/CreateBoard/CreateBoard.jsx
+++ frontend/src/components/Home/CreateBoard/CreateBoard.jsx
@@ -2,20 +2,35 @@
 jsx-a11y/no-static-element-interactions,
 jsx-a11y/click-events-have-key-events */
 
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button } from 'react-bootstrap';
 
+import AppContext from '../../../AppContext';
 import FormGroup from '../../_shared/FormGroup/FormGroup';
 import inputType from '../../../misc/inputType';
+import { postBoard } from '../../../misc/api';
 
 import logo from './createboard.svg';
 import './createboard.sass';
 
 const CreateBoard = ({ toggleOff }) => {
-  const [title, setTitle] = useState('');
+  const { currentUser, loadBoard } = useContext(AppContext);
+  const [name, setName] = useState('');
 
-  const handleSubmit = () => console.log('TODO: IMPLEMENT');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    postBoard({ name, team_id: currentUser.teamId })
+      .then((res) => {
+        toggleOff();
+        loadBoard(res.data.id);
+      })
+      .catch((err) => {
+        // TODO: Toast
+        console.error(err);
+      });
+  };
 
   return (
     <div className="CreateBoard" onClick={toggleOff}>
@@ -31,8 +46,8 @@ const CreateBoard = ({ toggleOff }) => {
         <FormGroup
           type={inputType.TEXT}
           label="title"
-          value={title}
-          setValue={setTitle}
+          value={name}
+          setValue={setName}
         />
 
         <div className="ButtonWrapper">
