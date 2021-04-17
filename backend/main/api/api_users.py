bbb3724a0d477947f7d5be16d98499e8c49787bf
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-17 09:35:35 +0100

    Authentication cases are added to the Get Users test suite, and they all pass.

--- backend/main/api/api_users.py
+++ backend/main/api/api_users.py
@@ -2,11 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import ErrorDetail
 from ..models import User
-from ..util import validate_team_id
+from ..util import validate_team_id, authenticate
 
 
 @api_view(['GET'])
 def users(request):
+    username = request.META.get('HTTP_AUTH_USER')
+    token = request.META.get('HTTP_AUTH_TOKEN')
+
+    authentication_response = authenticate(username, token)
+    if authentication_response:
+        return authentication_response
+
     team_id = request.query_params.get('team_id')
 
     validation_response = validate_team_id(team_id)
--- backend/main/tests/test_getusers.py
+++ backend/main/tests/test_getusers.py
@@ -1,6 +1,7 @@
 from rest_framework.test import APITestCase
 from rest_framework.exceptions import ErrorDetail
 from ..models import Team, User
+from ..util import not_authenticated_response, new_member
 
 
 class GetUsersTests(APITestCase):
@@ -12,13 +13,13 @@ class GetUsersTests(APITestCase):
             User.objects.create(
                 username=f'User #{i}',
                 password=b'$2b$12$DKVJHUAQNZqIvoi.OMN6v.x1ZhscKhbzSxpOBMykHgTI'
-                         b'MeeJpC6me',
+                         b'MeeJpC6m',
                 is_admin=False,
                 team=self.team
             ) for i in range(0, 3)
         ]
-        self.token = '$2b$12$qNhh2i1ZPU1qaIKncI7J6O2kr4XmuCWSwLEMJF653vyvDMI' \
-                     'RekzLO'
+        self.token = '$2b$12$WLmxQnf9kbDoW/8jA6kfIO9TfchCiGphBpckS2oy755wtdT' \
+                     'aIQsoq'
 
     def test_success(self):
         response = self.client.get(f'{self.endpoint}{self.team.id}',
@@ -59,3 +60,33 @@ class GetUsersTests(APITestCase):
             'team_id': ErrorDetail(string='Team not found.',
                                    code='not_found')
         })
+
+
+    def test_auth_user_empty(self):
+        response = self.client.get(f'{self.endpoint}{self.team.id}',
+                                   HTTP_AUTH_USER='',
+                                   HTTP_AUTH_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+    def test_auth_user_invalid(self):
+        response = self.client.get(f'{self.endpoint}{self.team.id}',
+                                   HTTP_AUTH_USER='invalidusername',
+                                   HTTP_AUTH_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+    def test_auth_token_empty(self):
+        response = self.client.get(f'{self.endpoint}{self.team.id}',
+                                   HTTP_AUTH_USER=self.users[0].username,
+                                   HTTP_AUTH_TOKEN='')
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
+    def test_auth_token_invalid(self):
+        response = self.client.get(f'{self.endpoint}{self.team.id}',
+                                   HTTP_AUTH_USER=self.users[0].username,
+                                   HTTP_AUTH_TOKEN='ASDKFJ!FJ_012rjpiwajfosia')
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data, not_authenticated_response.data)
+
