05bae46a8165190e6306c83065939f4d409b9407
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-17 09:22:36 +0100

    Team ID Blank case of the Get Users tests pass.

--- backend/main/api/api_users.py
+++ backend/main/api/api_users.py
@@ -1,11 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import ErrorDetail
 from ..models import User
 
 
 @api_view(['GET'])
 def users(request):
     team_id = request.query_params.get('team_id')
+    if not team_id:
+        return Response({
+            'team_id': ErrorDetail(string='Team ID cannot be empty.',
+                                   code='blank')
+        }, 400)
+
     users_queryset = User.objects.filter(team_id=team_id)
     return Response(list(map(
         lambda user: {'username': user.username},
