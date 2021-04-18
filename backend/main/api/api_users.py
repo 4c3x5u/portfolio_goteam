a2264a61ffc9a25c31e61f0f051995e14c9aed7d
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-18 11:41:17 +0100

    Success case of the Post User tests passes.

--- backend/main/api/api_users.py
+++ backend/main/api/api_users.py
@@ -4,32 +4,47 @@ from ..models import User, Board
 from ..util import validate_team_id, validate_board_id, authenticate
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def users(request):
-    username = request.META.get('HTTP_AUTH_USER')
-    token = request.META.get('HTTP_AUTH_TOKEN')
+    auth_user = request.META.get('HTTP_AUTH_USER')
+    auth_token = request.META.get('HTTP_AUTH_TOKEN')
 
-    authentication_response = authenticate(username, token)
+    authentication_response = authenticate(auth_user, auth_token)
     if authentication_response:
         return authentication_response
 
-    team_id = request.query_params.get('team_id')
-    validation_response = validate_team_id(team_id)
-    if validation_response:
-        return validation_response
+    if request.method == 'GET':
+        team_id = request.query_params.get('team_id')
+        validation_response = validate_team_id(team_id)
+        if validation_response:
+            return validation_response
 
-    board_id = request.query_params.get('board_id')
-    board, validation_response = validate_board_id(board_id)
-    if validation_response:
-        return validation_response
+        board_id = request.query_params.get('board_id')
+        board, validation_response = validate_board_id(board_id)
+        if validation_response:
+            return validation_response
 
-    users_list = User.objects.filter(team_id=team_id)
-    board_users = User.objects.filter(board=board)
+        users_list = User.objects.filter(team_id=team_id)
+        board_users = User.objects.filter(board=board)
 
-    return Response(list(map(
-        lambda user: {
-            'username': user.username,
-            'isActive': user in board_users
-        },
-        users_list
-    )), 200)
+        return Response(list(map(
+            lambda user: {'username': user.username,
+                          'isActive': user in board_users},
+            users_list
+        )), 200)
+
+    if request.method == 'POST':
+        username = request.data.get('username')
+        board_id = request.data.get('board_id')
+        is_active = request.data.get('is_active')
+
+        user = User.objects.get(username=username)
+        board = Board.objects.get(id=board_id)
+
+        if is_active == 'True':
+            board.user.add(user)
+        else:
+            board.user.remove(user)
+
+        return Response({'msg': f'{username} is removed from {board.name}.'},
+                        200)
