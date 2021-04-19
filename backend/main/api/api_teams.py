337d4f5f2050ab5ce84fae5f9b9cca9d6c629e1d
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-19 02:49:38 +0100

    Team ID validation tests are added to the Get Team tests, and they all pass.

--- backend/main/api/api_teams.py
+++ backend/main/api/api_teams.py
@@ -5,7 +5,7 @@ from ..util import validate_team_id
 
 @api_view(['GET'])
 def teams(request):
-    team_id = request.query_params.get('id')
+    team_id = request.query_params.get('team_id')
     team, validation_response = validate_team_id(team_id)
     if validation_response:
         return validation_response
