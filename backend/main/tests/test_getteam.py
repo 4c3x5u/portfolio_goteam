4de001aa72d0affa8d06d5a7d8cec7c7232076e2
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-19 02:49:38 +0100

    Team ID validation tests are added to the Get Team tests, and they all pass.

--- backend/main/tests/test_getteam.py
+++ backend/main/tests/test_getteam.py
@@ -1,10 +1,11 @@
 from rest_framework.test import APITestCase
+from rest_framework.exceptions import ErrorDetail
 from ..models import Team
 from ..util import new_admin
 
 
 class GetTeamTests(APITestCase):
-    endpoint = '/teams/?id='
+    endpoint = '/teams/?team_id='
 
     def setUp(self):
         self.team = Team.objects.create()
@@ -17,3 +18,29 @@ class GetTeamTests(APITestCase):
             'id': self.team.id,
             'inviteCode': self.team.invite_code
         })
+
+    def test_team_id_empty(self):
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, {
+            'team_id': ErrorDetail(string='Team ID cannot be empty.',
+                                   code='blank')
+        })
+
+    def test_team_id_invalid(self):
+        response = self.client.get(f'{self.endpoint}asdfsa')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, {
+            'team_id': ErrorDetail(string='Team ID must be a number.',
+                                   code='invalid')
+        })
+
+
+    def test_team_not_found(self):
+        response = self.client.get(f'{self.endpoint}12314241')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, {
+            'team_id': ErrorDetail(string='Team not found.',
+                                   code='not_found')
+        })
+
