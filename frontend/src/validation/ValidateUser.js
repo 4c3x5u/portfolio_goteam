146c2b8f188bec450b5794f9c6b8aff401de7d44
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-20 15:59:26 +0100

    Duplicate code from the validation objects is extracted into a new file named Validate.

--- frontend/src/validation/ValidateUser.js
+++ frontend/src/validation/ValidateUser.js
@@ -1,29 +1,15 @@
+import Validate from './Validate';
+
 const ValidateUser = {
-  username: (username) => {
-    if (!username) {
-      return 'Username cannot be empty.';
-    } if (username.length < 5) {
-      return 'Username cannot be shorter than 5 characters.';
-    } if (username.length > 35) {
-      return 'Username cannot be longer than 35 characters.';
-    } return '';
-  },
-  password: (password) => {
-    if (!password) {
-      return 'Password cannot be empty.';
-    } if (password.length < 8) {
-      return 'Password cannot be shorter than 8 characters.';
-    } if (password.length > 255) {
-      return 'Password cannot be longer than 255 characters.';
-    } return '';
-  },
+  username: Validate.requiredString('username', 35, 5),
+  password: Validate.requiredString('password', 255, 8),
   passwordConfirmation: (passwordConfirmation, password) => {
-    if (!passwordConfirmation) {
-      return 'Confirmation cannot be empty.';
-    } if (passwordConfirmation.length < 8) {
-      return 'Confirmation cannot be shorter than 8 characters.';
-    } if (passwordConfirmation.length > 255) {
-      return 'Confirmation cannot be longer than 255 characters.';
+    const validationError = (
+      Validate.requiredString('confirmation', 255, 8)(passwordConfirmation)
+    );
+
+    if (validationError) {
+      return validationError;
     } if (password && password !== passwordConfirmation) {
       return 'Confirmation must match the password.';
     } return '';
