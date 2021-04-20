a8fbc8da2ee33b40e589430f71ecfc9b0cc8154a
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-20 17:41:04 +0100

    Client and server-side validation and error handling implemented for Create Task/Subtask components.

--- frontend/src/components/Home/CreateTask/AddSubtasks/AddSubtasks.jsx
+++ frontend/src/components/Home/CreateTask/AddSubtasks/AddSubtasks.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form } from 'react-bootstrap';
 
@@ -6,62 +6,83 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import './addsubtasks.sass';
+import ValidateSubtask from '../../../../validation/ValidateSubtask';
 
-const AddSubtasks = ({ subtasks, setSubtasks }) => (
-  <Form.Group className="AddSubtasks">
-    <Form.Label className="Label">
-      SUBTASKS
-    </Form.Label>
+const AddSubtasks = ({ subtasks, setSubtasks }) => {
+  const [error, setError] = useState('');
 
-    {subtasks.list.map((subtask, index) => (
-      <div className="ControlWrapper">
+  const addSubtask = () => {
+    const titleError = ValidateSubtask.title(subtasks.value);
+
+    if (titleError) {
+      setError(titleError);
+    } else {
+      setSubtasks({
+        value: '',
+        list: [...subtasks.list, subtasks.value],
+      });
+    }
+  };
+
+  return (
+    <div className="AddSubtasks">
+      <Form.Label className="Label">
+        SUBTASKS
+      </Form.Label>
+
+      {subtasks.list.map((subtask, index) => (
+        <div className="ControlWrapper">
+          <Form.Control
+            key={subtask}
+            className="Input"
+            type="text"
+            value={subtask}
+            onChange={() => console.log('NOT ALLOWED')}
+          />
+
+          <Button
+            className="Remove"
+            onClick={() => {
+              setSubtasks({
+                value: '',
+                list: subtasks.list.filter((_, i) => i !== index),
+              });
+            }}
+            type="button"
+          >
+            <FontAwesomeIcon className="Icon" icon={faTimes} />
+          </Button>
+        </div>
+      ))}
+
+      <Form.Group className="ControlWrapper">
         <Form.Control
-          key={subtask}
           className="Input"
           type="text"
-          value={subtask}
-          onChange={() => console.log('NOT ALLOWED')}
+          value={subtasks.value}
+          onChange={(e) => setSubtasks({
+            ...subtasks,
+            value: e.target.value,
+          })}
         />
 
         <Button
-          className="Remove"
-          onClick={() => {
-            setSubtasks({
-              value: '',
-              list: subtasks.list.filter((_, i) => i !== index),
-            });
-          }}
+          className="Add"
+          onClick={addSubtask}
           type="button"
         >
-          <FontAwesomeIcon className="Icon" icon={faTimes} />
+          <FontAwesomeIcon className="Icon" icon={faPlus} />
         </Button>
-      </div>
-    ))}
 
-    <div className="ControlWrapper">
-      <Form.Control
-        className="Input"
-        type="text"
-        value={subtasks.value}
-        onChange={(e) => setSubtasks({
-          ...subtasks,
-          value: e.target.value,
-        })}
-      />
-
-      <Button
-        className="Add"
-        onClick={() => setSubtasks({
-          value: '',
-          list: [...subtasks.list, subtasks.value],
-        })}
-        type="button"
-      >
-        <FontAwesomeIcon className="Icon" icon={faPlus} />
-      </Button>
+        {error && (
+          <span className="Error text-danger">
+            {error}
+          </span>
+        )}
+      </Form.Group>
     </div>
-  </Form.Group>
-);
+  );
+};
 
 AddSubtasks.propTypes = {
   subtasks: PropTypes.objectOf({
