d7f3f9a5c4d82938eb1db5b8ce9b5b69e69d7a1c
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-20 17:41:04 +0100

    Client and server-side validation and error handling implemented for Create Task/Subtask components.

--- frontend/src/components/Home/CreateTask/CreateTask.jsx
+++ frontend/src/components/Home/CreateTask/CreateTask.jsx
@@ -12,29 +12,51 @@ import AppContext from '../../../AppContext';
 import TasksAPI from '../../../api/TasksAPI';
 import FormGroup from '../../_shared/FormGroup/FormGroup';
 import AddSubtasks from './AddSubtasks/AddSubtasks';
+// import ValidateTask from '../../../validation/ValidateTask';
 import inputType from '../../../misc/inputType';
 
 import logo from './createtask.svg';
 import './createtask.sass';
 
 const CreateTask = ({ toggleOff }) => {
-  const { activeBoard, loadBoard } = useContext(AppContext);
+  const { activeBoard, loadBoard, notify } = useContext(AppContext);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [subtasks, setSubtasks] = useState({ value: '', list: [] });
+  const [titleError, setTitleError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    TasksAPI
-      .post({
-        title,
-        description,
-        column: activeBoard.columns[0].id,
-        subtasks: subtasks.list,
-      })
-      .then(() => { loadBoard(); toggleOff(); })
-      .catch((err) => console.error(err)); // TODO: Toast
+    const clientTitleError = ValidateTask.title(title);
+
+    if (clientTitleError) {
+      setTitleError(clientTitleError);
+    } else {
+      TasksAPI
+        .post({
+          title,
+          description,
+          column: activeBoard.columns[0].id,
+          subtasks: subtasks.list,
+        })
+        .then(() => {
+          loadBoard();
+          toggleOff();
+        })
+        .catch((err) => {
+          const serverTitleError = err?.response?.data?.title || '';
+
+          if (serverTitleError) {
+            setTitleError(serverTitleError);
+          } else {
+            notify(
+              'Unable to create task.',
+              `${err?.message || 'Server Error'}.`,
+            );
+          }
+        });
+    }
   };
 
   return (
@@ -53,6 +75,7 @@ const CreateTask = ({ toggleOff }) => {
           label="title"
           value={title}
           setValue={setTitle}
+          error={titleError}
         />
 
         <FormGroup
@@ -62,7 +85,10 @@ const CreateTask = ({ toggleOff }) => {
           setValue={setDescription}
         />
 
-        <AddSubtasks subtasks={subtasks} setSubtasks={setSubtasks} />
+        <AddSubtasks
+          subtasks={subtasks}
+          setSubtasks={setSubtasks}
+        />
 
         <Row className="ButtonWrapper">
           <Col className="ButtonCol">
