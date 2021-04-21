d4c35b647f4079ec6422caf546eecc6c8c412c3d
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-21 08:34:23 +0100

    Project link updated for the footer.

--- frontend/src/components/Home/Footer/Footer.jsx
+++ frontend/src/components/Home/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => (
       <Row className="Row">
         <Col className="Col" xs={6}>
           <a
-            href="https://github.com/4c3x5u/portfolio_goteam"
+            href="https://github.com/alicandev/portfolio_goteam"
             target="_blank"
             rel="noreferrer"
           >
