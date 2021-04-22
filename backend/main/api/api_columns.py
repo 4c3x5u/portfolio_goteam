2387408ad52d2f57771aa8194b9ff31c66e82c14
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-22 11:06:11 +0100

    Wrong Team case passes for Get Columns tests.

--- backend/main/api/api_columns.py
+++ backend/main/api/api_columns.py
@@ -4,7 +4,9 @@ from rest_framework.exceptions import ErrorDetail
 from ..models import Column, Board, Task
 from ..serializers.ser_column import ColumnSerializer
 from ..serializers.ser_task import TaskSerializer
-from ..validation.val_auth import authenticate, authorize
+from ..validation.val_auth import \
+    authenticate, authorize, not_authenticated_response
+from ..validation.val_board import validate_board_id
 
 
 @api_view(['GET', 'PATCH'])
@@ -12,37 +14,19 @@ def columns(request):
     username = request.META.get('HTTP_AUTH_USER')
     token = request.META.get('HTTP_AUTH_TOKEN')
 
-    user, authentication_response = authenticate(username, token)
+    team_id, authentication_response = authenticate(username, token)
     if authentication_response:
         return authentication_response
 
     if request.method == 'GET':
         board_id = request.query_params.get('board_id')
-
-        if not board_id:
-            return Response({
-                'board_id': ErrorDetail(string='Board ID cannot be empty.',
-                                        code='blank')
-            }, 400)
-
-        try:
-            int(board_id)
-        except ValueError:
-            return Response({
-                'board_id': ErrorDetail(string='Board ID must be a number.',
-                                        code='invalid')
-            }, 400)
-
-        try:
-            Board.objects.get(id=board_id)
-        except Board.DoesNotExist:
-            return Response({
-                'board_id': ErrorDetail(string='Board not found.',
-                                        code='not_found')
-            }, 404)
+        board, validation_response = validate_board_id(board_id)
+        if validation_response:
+            return validation_response
+        if board.team.id != team_id:
+            return not_authenticated_response
 
         board_columns = Column.objects.filter(board_id=board_id)
-
         if not board_columns:
             board_columns = [
                 Column.objects.create(
@@ -52,7 +36,6 @@ def columns(request):
             ]
 
         serializer = ColumnSerializer(board_columns, many=True)
-
         return Response({
             'columns': list(
                 map(lambda column: {'id': column['id'], 'order': column['order']},
