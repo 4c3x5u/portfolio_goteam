c19532a485b44e2b4c41bf0b4b3b92a4d3004ec1
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-23 16:33:59 +0100

    `onContextMenu` event's default behaviour is prevent app-wide.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
