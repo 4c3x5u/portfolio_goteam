a866eb28855527ba0dad76bf4d082e9da8542899
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-23 17:01:16 +0100

    Assign User cases pass for Patch Task tests.

--- backend/main/serializers/ser_task.py
+++ backend/main/serializers/ser_task.py
@@ -3,12 +3,19 @@ from ..models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=50, error_messages={
-        'blank': 'Title cannot be empty.',
-        'max_length': 'Title cannot be longer than 50 characters.'
-    })
-    description = serializers.CharField(allow_blank=True)
-
     class Meta:
         model = Task
         fields = '__all__'
+        extra_kwargs = {
+            'user': {
+                'error_messages': {
+                    'does_not_exist': 'User does not exist.'
+                }
+            },
+            'title': {
+                'error_messages': {
+                    'blank': 'Title cannot be empty.',
+                    'max_length': 'Title cannot be longer than 50 characters.'
+                }
+            }
+        }
