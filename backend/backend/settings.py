01cdcc6287e914361f935137b971bdb5d7eb0a3e
Alican Demirtas <demirtasdev@hotmail.com>
2021-04-30 10:24:40 +0100

    Default Database set back to PostgreSQL.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': f'{BASE_DIR}/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goteam',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
