a2081b44873882b3cf704c0fc177a32bc682af59
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-08 20:19:33 +0100

    Function-based Subtask API view is refactored as a class-based view.

--- backend/main/validation/val_auth.py
+++ backend/main/validation/val_auth.py
@@ -2,15 +2,18 @@ from main.models import User
 from rest_framework.exceptions import ErrorDetail
 from rest_framework.response import Response
 import bcrypt
+import status
+from .val_custom import CustomAPIException
 
 
+# TODO: delete once you moved on to the customvalidation approach
 not_authenticated_response = Response({
     'auth': ErrorDetail(string="Authentication failure.",
                         code='not_authenticated')
 }, 403)
 
 
-# return (team id, response)
+# TODO: delete once you moved on to the customvalidation approach
 def authenticate(username, token):
     try:
         user = User.objects.get(username=username)
@@ -28,14 +31,14 @@ def authenticate(username, token):
 
     return user, None
 
-
+# TODO: delete once you moved on to the customvalidation approach
 not_authorized_response = Response({
     'auth': ErrorDetail(string='Authorization failure.',
                         code='not_authorized')
 }, 403)
 
 
-# return response
+# TODO: delete once you moved on to the customvalidation approach
 def authorize(username):
     try:
         user = User.objects.get(username=username)
@@ -43,3 +46,43 @@ def authorize(username):
             return not_authorized_response
     except User.DoesNotExist:
         return not_authorized_response
+
+
+authentication_error = CustomAPIException('auth',
+                                        'Authentication failure.',
+                                          status.HTTP_403_FORBIDDEN)
+
+
+def authenticate_custom(username, token):
+    try:
+        user = User.objects.get(username=username)
+    except (User.DoesNotExist, ValueError):
+        return None, authentication_error
+
+    try:
+        tokens_match = bcrypt.checkpw(
+            bytes(user.username, 'utf-8') + user.password,
+            bytes(token, 'utf-8'))
+        if not tokens_match:
+            return None, authentication_error
+    except ValueError:
+        return None, authentication_error
+
+    return user, None
+
+
+authorization_error = CustomAPIException(
+    'auth',
+    'Authorization failure.',
+    status.HTTP_401_UNAUTHORIZED
+)
+
+
+# TODO: rename as authorize once you moved on to the customvalidation approach
+def authorize_custom(username):
+    try:
+        user = User.objects.get(username=username)
+        if not user.is_admin:
+            return authorization_error
+    except User.DoesNotExist:
+        return authorization_error
