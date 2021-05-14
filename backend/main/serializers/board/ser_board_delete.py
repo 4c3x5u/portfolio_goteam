11131ebb7e5d3fc06cca075c75dfaa1de6817b3b
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-14 12:34:17 +0100

    A bug that arises when an admin deletes the last remaining board is fixed.

--- backend/main/serializers/board/ser_board_delete.py
+++ backend/main/serializers/board/ser_board_delete.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
+import status
 
 from main.serializers.board.ser_board import BoardSerializer
 from main.validation.val_auth import authenticate, authorize
+from main.validation.val_custom import CustomAPIException
 from main.models import Board
 
 
 class DeleteBoardSerializer(serializers.ModelSerializer):
     board = serializers.PrimaryKeyRelatedField(
-        queryset=Board.objects.all(),
+        queryset=Board.objects.prefetch_related('team__board_set').all(),
         error_messages={'null': 'Board ID cannot be null.',
                         'incorrect_type': 'Board ID must be a number.',
                         'does_not_exist': 'Board does not exist.'}
@@ -22,6 +24,11 @@ class DeleteBoardSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         user = authenticate(attrs.get('auth_user'), attrs.get('auth_token'))
         board = attrs.get('board')
+        if len(board.team.board_set.all()) <= 1:
+            raise CustomAPIException(
+                'board',
+                'You cannot delete the last remaining board.',
+                status.HTTP_400_BAD_REQUEST)
         authorize(user, board.team_id)
         return board
 
