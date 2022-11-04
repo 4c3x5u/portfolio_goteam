fbf7325bee4450de18a9ccbb121ddc89aaada230
Atlas Kaplan <kxplxn@icloud.com>
2022-11-04 16:37:35 +0000

    Username validation tests are concatenated into a table-driven tests.

--- server-v2/api/register_test.go
+++ server-v2/api/register_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -13,73 +14,55 @@ import (
 )
 
 func TestRegister(t *testing.T) {
-	t.Run("Username Validation", func(t *testing.T) {
-		t.Run("Too Short", func(t *testing.T) {
-			// arrange
-			req, err := http.NewRequest("POST", "/register", strings.NewReader(`{
-				"username": "bob", 
-				"password": "securepass1!", 
-				"referrer": ""
-			}`))
-			if err != nil {
-				t.Fatal(err)
-			}
-			req.Header.Set("Content-Type", "application/json")
-			w := httptest.NewRecorder()
-			handler := NewHandlerRegister(relay.NewAPILogger())
-			wantErrs := []string{"Username cannot be shorter than 5 characters."}
+	t.Run("UsernameValidation", func(t *testing.T) {
+		for _, c := range []struct {
+			caseName string
+			username string
+			wantErrs []string
+		}{
+			{
+				caseName: "TooShort",
+				username: "bob",
+				wantErrs: []string{"Username cannot be shorter than 5 characters."},
+			},
+			{
+				caseName: "TooLong",
+				username: "bobobobobobobobob",
+				wantErrs: []string{"Username cannot be longer than 15 characters."},
+			},
+		} {
+			t.Run(c.caseName, func(t *testing.T) {
+				// arrange
+				req, err := http.NewRequest("POST", "/register", strings.NewReader(fmt.Sprintf(`{
+					"username": "%s", 
+					"password": "securepass1!", 
+					"referrer": ""
+				}`, c.username)))
+				if err != nil {
+					t.Fatal(err)
+				}
+				req.Header.Set("Content-Type", "application/json")
+				w := httptest.NewRecorder()
+				handler := NewHandlerRegister(relay.NewAPILogger())
 
-			// act
-			handler.ServeHTTP(w, req)
+				// act
+				handler.ServeHTTP(w, req)
 
-			// assert
-			res := w.Result()
-			if res.StatusCode != http.StatusBadRequest {
-				t.Logf("\nwant: %d\ngot: %d", http.StatusBadRequest, res.StatusCode)
-				t.Fail()
-			}
-			resBody := &ResRegister{}
-			if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
-				t.Fatal(err)
-			}
-			if !cmp.Equal(resBody.Errs.Username, wantErrs) {
-				t.Logf("\nwant: %+v\ngot: %+v", wantErrs, resBody.Errs.Username)
-				t.Fail()
-			}
-		})
-
-		t.Run("Too Long", func(t *testing.T) {
-			// arrange
-			req, err := http.NewRequest("POST", "/register", strings.NewReader(`{
-				"username": "bobobobobobobbob", 
-				"password": "securepass1!", 
-				"referrer": ""
-			}`))
-			if err != nil {
-				t.Fatal(err)
-			}
-			req.Header.Set("Content-Type", "application/json")
-			w := httptest.NewRecorder()
-			handler := NewHandlerRegister(relay.NewAPILogger())
-			wantErrs := []string{"Username cannot be longer than 15 characters."}
-
-			// act
-			handler.ServeHTTP(w, req)
-
-			// assert
-			res := w.Result()
-			if res.StatusCode != http.StatusBadRequest {
-				t.Logf("\nwant: %d\ngot: %d", http.StatusBadRequest, res.StatusCode)
-				t.Fail()
-			}
-			resBody := &ResRegister{}
-			if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
-				t.Fatal(err)
-			}
-			if !cmp.Equal(resBody.Errs.Username, wantErrs) {
-				t.Logf("\nwant: %+v\ngot: %+v", wantErrs, resBody.Errs.Username)
-				t.Fail()
-			}
-		})
+				// assert
+				res := w.Result()
+				if res.StatusCode != http.StatusBadRequest {
+					t.Logf("\nwant: %d\ngot: %d", http.StatusBadRequest, res.StatusCode)
+					t.Fail()
+				}
+				resBody := &ResRegister{}
+				if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(resBody.Errs.Username, c.wantErrs) {
+					t.Logf("\nwant: %+v\ngot: %+v", c.wantErrs, resBody.Errs.Username)
+					t.Fail()
+				}
+			})
+		}
 	})
 }
