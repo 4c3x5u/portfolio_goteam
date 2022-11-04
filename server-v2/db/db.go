94836ec4dd947c2691ac7b38a747164771aea01f
Atlas Kaplan <kxplxn@icloud.com>
2022-11-04 12:25:44 +0000

    Code comments are added to Package db.

--- server-v2/db/db.go
+++ server-v2/db/db.go
@@ -9,15 +9,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-const (
-	ConnStr = "DBCONNSTR"
-)
+// ConnStr declares the name of the environment variable to retrieve for the
+// database connection string.
+const ConnStr = "DBCONNSTR"
 
-func Close(
-	client *mongo.Client,
-	ctx context.Context,
-	cancel context.CancelFunc,
-) {
+// Close closes the database connection.
+func Close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
 	defer cancel()
 
 	defer func() {
@@ -27,17 +24,15 @@ func Close(
 	}()
 }
 
-func Connect(uri string) (
-	*mongo.Client,
-	context.Context,
-	context.CancelFunc,
-	error,
-) {
+// Connect sets up a database connection using a mongo db uri.
+func Connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	return client, ctx, cancel, err
 }
 
+// Ping verifies that the database connection is successful — returns an error
+// if not.
 func Ping(client *mongo.Client, ctx context.Context) error {
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		return err
