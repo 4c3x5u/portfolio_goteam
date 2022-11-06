76b8cfe89f20c6525916374468f3f910385b9935
Atlas Kaplan <kxplxn@icloud.com>
2022-11-06 21:48:06 +0000

    #31 EqualArr function now uses the built-in comparable interface.

--- server-v2/assert/assert.go
+++ server-v2/assert/assert.go
@@ -1,14 +1,8 @@
 package assert
 
-// Comparable is a generic type constraint for all comparable data types in Go.
-type Comparable interface {
-	~string | ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 |
-		~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
-}
-
 // EqualArr compares two arrays and returns a boolean based on whether or not
 // they contain exactly the same items.
-func EqualArr[T Comparable](a []T, b []T) bool {
+func EqualArr[T comparable](a []T, b []T) bool {
 	if len(a) != len(b) {
 		return false
 	}
