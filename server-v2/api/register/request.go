0cf74aacdd48c005e008f0a9b23615ed85e990a9
Atlas Kaplan <kxplxn@icloud.com>
2022-11-10 23:44:09 +0000

    Multiple-field errors are enabled on the register API route.

--- server-v2/api/register/request.go
+++ server-v2/api/register/request.go
@@ -8,22 +8,16 @@ type ReqBody struct {
 }
 
 // Validate uses individual field validation logic defined in the validation.go
-// file to validate requests sent the register route. It returns false and
-// an errors object if any of the individual validations fail.
+// file to validate requests sent the register route. It returns an errors
+// object if any of the individual validations fail.
 func (r *ReqBody) Validate() *Errs {
 	errs := &Errs{}
 
-	// validate username
-	if errsUsername := r.Username.Validate(); errsUsername != nil {
-		errs.Username = errsUsername
+	errs.Username = r.Username.Validate()
+	errs.Password = r.Password.Validate()
+
+	if errs.Username != nil || errs.Password != nil {
 		return errs
 	}
-
-	// validate password
-	if errsPassword := r.Password.Validate(); errsPassword != nil {
-		errs.Password = errsPassword
-		return errs
-	}
-
 	return nil
 }
