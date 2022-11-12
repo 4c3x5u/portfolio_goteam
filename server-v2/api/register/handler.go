65c1837d581e97924b323f25e23195ac65d387f4
Atlas Kaplan <kxplxn@icloud.com>
2022-11-12 15:57:10 +0000

    #39 Package relay is extended to remove the duplication between validation error returns in the register handler.

--- server-v2/api/register/handler.go
+++ server-v2/api/register/handler.go
@@ -28,7 +28,7 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// decode body into request object
 	req := &ReqBody{}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		relay.ErrAPIInternal(w, err.Error())
+		relay.ServerErr(w, err.Error())
 		return
 	}
 
@@ -38,29 +38,21 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// validate the request
 	if errs := req.Validate(); errs != nil {
 		res.ErrsValidation = errs
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		if err := json.NewEncoder(w).Encode(res); err != nil {
-			relay.ErrAPIInternal(w, err.Error())
-		}
+		relay.ClientJSON(w, res, http.StatusBadRequest)
 		return
 	}
 
 	errsValidation, err := h.creatorUser.CreateUser(string(req.Username), string(req.Password))
 	if err != nil {
-		relay.ErrAPIInternal(w, err.Error())
+		relay.ServerErr(w, err.Error())
 		return
 	}
 	if errsValidation != nil {
 		res.ErrsValidation = errsValidation
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		if err := json.NewEncoder(w).Encode(res); err != nil {
-			relay.ErrAPIInternal(w, err.Error())
-		}
+		relay.ClientJSON(w, res, http.StatusBadRequest)
 		return
 	}
 
-	relay.ErrAPIInternal(w, "not implemented")
+	relay.ServerErr(w, "not implemented")
 	return
 }
--- server-v2/relay/relay.go
+++ server-v2/relay/relay.go
@@ -3,13 +3,24 @@
 package relay
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-// ErrAPIInternal relays an internal API error by logging a message to the
-// console and returning 500 to the user.
-func ErrAPIInternal(w http.ResponseWriter, msg string) {
+// ServerErr relays an internal server error by logging a message to the console
+// and returning 500 to the user.
+func ServerErr(w http.ResponseWriter, msg string) {
 	log.Printf("ERROR: %s", msg)
 	w.WriteHeader(http.StatusInternalServerError)
+}
+
+// ClientJSON relays a JSON object to the client by writing it to the HTTP
+// response body as well as writing the specified status code into the header.
+func ClientJSON(w http.ResponseWriter, res any, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		ServerErr(w, err.Error())
+	}
 }
