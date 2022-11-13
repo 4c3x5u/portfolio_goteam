c82990c084d83e7e7b968b54a969460c8febd094
Atlas Kaplan <kxplxn@icloud.com>
2022-11-13 23:57:36 +0000

    #35 ErrsMap method is removed from ResBody as redundant.

--- server-v2/api/register/response.go
+++ server-v2/api/register/response.go
@@ -5,15 +5,6 @@ type ResBody struct {
 	ErrsValidation *ErrsValidation `json:"errors"`
 }
 
-// ErrsMap implements the test.ErrsMapper interface to enable test cases to
-// inspect the errors object through dynamically defined field names.
-func (r *ResBody) ErrsMap() map[string][]string {
-	errsMap := make(map[string][]string)
-	errsMap["password"] = r.ErrsValidation.Username
-	errsMap["password"] = r.ErrsValidation.Password
-	return errsMap
-}
-
 // ErrsValidation defines the structure of error object that can be encoded in
 // the register route in the case of an error.
 type ErrsValidation struct {
