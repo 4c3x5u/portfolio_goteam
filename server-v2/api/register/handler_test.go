8c62180775b251ff247ba96f6bfebcfb2176175e
Atlas Kaplan <kxplxn@icloud.com>
2022-11-14 01:36:51 +0000

    #40 Cleanup.

--- server-v2/api/register/handler_test.go
+++ server-v2/api/register/handler_test.go
@@ -10,102 +10,87 @@ import (
 	"github.com/kxplxn/goteam/server-v2/assert"
 )
 
+// todo: move to db tests
+const usnTaken = "Username is already taken."
+
 func TestHandler(t *testing.T) {
-	t.Run("Errs", func(t *testing.T) {
-		const (
-			usnTooShort = "Username cannot be shorter than 5 characters."
-			usnTooLong  = "Username cannot be longer than 15 characters."
-			usnTaken    = "Username is already taken."
+	// handler setup
+	creator, validator := &fakeCreatorUser{}, &fakeValidatorReq{}
+	sut := NewHandler(creator, validator)
 
-			pwdTooShort = "Password cannot be shorter than 8 characters."
-			pwdNoDigit  = "Password must contain a digit (0-9)."
-		)
+	// test cases below should all return 400
+	wantStatusCode := http.StatusBadRequest
 
-		// handler setup
-		creator, validator := &fakeCreatorUser{}, &fakeValidatorReq{}
-		sut := NewHandler(creator, validator)
+	// test cases
+	for _, c := range []struct {
+		name          string
+		reqBody       *ReqBody
+		errsValidator *Errs
+		errsCreator   *Errs
+		wantErrs      *Errs
+	}{
+		{
+			name:          "ErrsValidator",
+			reqBody:       &ReqBody{Username: "bobobobobobobobob", Password: "myNOdigitPASSWORD!"},
+			errsValidator: &Errs{Username: []string{usnTooLong}, Password: []string{pwdNoDigit}},
+			errsCreator:   nil,
+			wantErrs:      &Errs{Username: []string{usnTooLong}, Password: []string{pwdNoDigit}},
+		},
+		{
+			name:          "ErrsCreator",
+			reqBody:       &ReqBody{Username: "bob21", Password: "Myp4ssword!"},
+			errsValidator: nil,
+			errsCreator:   &Errs{Username: []string{usnTaken}},
+			wantErrs:      &Errs{Username: []string{usnTaken}},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			// parse response body - done only to assert tha the creator and
+			// the validator receives the correct input based on the request
+			// passed in
+			reqBody, err := json.Marshal(c.reqBody)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		// test cases below should all return 400
-		wantStatusCode := http.StatusBadRequest
+			// set the wantOutErrs return on fake validator and creator (arrange)
+			validator.outErrs = c.errsValidator
+			creator.outErrs = c.errsCreator
 
-		// test cases
-		for _, c := range []struct {
-			name          string
-			reqBody       *ReqBody
-			errsValidator *Errs
-			errsCreator   *Errs
-			wantErrs      *Errs
-		}{
-			{
-				name:          "Validator1",
-				reqBody:       &ReqBody{Username: "bob1", Password: "Myp4ss!"},
-				errsValidator: &Errs{Username: []string{usnTooShort}, Password: []string{pwdTooShort}},
-				errsCreator:   nil,
-				wantErrs:      &Errs{Username: []string{usnTooShort}, Password: []string{pwdTooShort}},
-			},
-			{
-				name:          "Validator2",
-				reqBody:       &ReqBody{Username: "bobobobobobobobob", Password: "myNOdigitPASSWORD!"},
-				errsValidator: &Errs{Username: []string{usnTooLong}, Password: []string{pwdNoDigit}},
-				errsCreator:   nil,
-				wantErrs:      &Errs{Username: []string{usnTooLong}, Password: []string{pwdNoDigit}},
-			},
-			{
-				name:          "Creator",
-				reqBody:       &ReqBody{Username: "bob21", Password: "Myp4ssword!"},
-				errsValidator: nil,
-				errsCreator:   &Errs{Username: []string{usnTaken}},
-				wantErrs:      &Errs{Username: []string{usnTaken}},
-			},
-		} {
-			t.Run(c.name, func(t *testing.T) {
-				// parse response body - done only to assert tha the creator and
-				// the validator receives the correct input based on the request
-				// passed in
-				reqBody, err := json.Marshal(c.reqBody)
-				if err != nil {
-					t.Fatal(err)
-				}
+			// create request (arrange)
+			req, err := http.NewRequest("POST", "/register", bytes.NewReader(reqBody))
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
 
-				// set the wantOutErrs return on fake validator and creator (arrange)
-				validator.outErrs = c.errsValidator
-				creator.outErrs = c.errsCreator
+			// send request (act)
+			sut.ServeHTTP(w, req)
 
-				// create request (arrange)
-				req, err := http.NewRequest("POST", "/register", bytes.NewReader(reqBody))
-				if err != nil {
-					t.Fatal(err)
-				}
-				req.Header.Set("Content-Type", "application/json")
-				w := httptest.NewRecorder()
+			// assert that the handler correctly passed in the arguments
+			// for request validator and user creator
+			assert.Equal(t, c.reqBody.Username, validator.inReqBody.Username)
+			assert.Equal(t, c.reqBody.Password, validator.inReqBody.Password)
 
-				// send request (act)
-				sut.ServeHTTP(w, req)
+			// When errors occur on validator, the handler code will
+			// terminate and creator will not be called, causing these
+			// assertions to fail. Only make them if the validator is set
+			// to return nil Errs.
+			if c.errsValidator == nil {
+				assert.Equal(t, c.reqBody.Username, creator.inUsername)
+				assert.Equal(t, c.reqBody.Password, creator.inPassword)
+			}
 
-				// assert that the handler correctly passed in the arguments
-				// for request validator and user creator
-				assert.Equal(t, c.reqBody.Username, validator.inReqBody.Username)
-				assert.Equal(t, c.reqBody.Password, validator.inReqBody.Password)
-
-				// When errors occur on validator, the handler code will
-				// terminate and creator will not be called, causing these
-				// assertions to fail. Only make them if the validator is set
-				// to return nil Errs.
-				if c.errsValidator == nil {
-					assert.Equal(t, c.reqBody.Username, creator.inUsername)
-					assert.Equal(t, c.reqBody.Password, creator.inPassword)
-				}
-
-				// make assertions on the status code and response body (assert)
-				res := w.Result()
-				assert.Equal(t, wantStatusCode, res.StatusCode)
-				resBody := &ResBody{}
-				if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
-					t.Fatal(err)
-				}
-				assert.EqualArr(t, c.wantErrs.Username, resBody.Errs.Username)
-				assert.EqualArr(t, c.wantErrs.Password, resBody.Errs.Password)
-			})
-		}
-	})
+			// make assertions on the status code and response body (assert)
+			res := w.Result()
+			assert.Equal(t, wantStatusCode, res.StatusCode)
+			resBody := &ResBody{}
+			if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+				t.Fatal(err)
+			}
+			assert.EqualArr(t, c.wantErrs.Username, resBody.Errs.Username)
+			assert.EqualArr(t, c.wantErrs.Password, resBody.Errs.Password)
+		})
+	}
 }
