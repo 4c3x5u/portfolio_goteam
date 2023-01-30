8f40ba30438d66b66687e8ba975edf8a5ee98a21
Atlas Kaplan <kxplxn@icloud.com>
2023-01-30 10:21:37 +0000

    #77 appLogger.go is renamed as logger.go

--- server/log/logger.go
+++ server/log/logger.go
@@ -2,6 +2,21 @@ package log
 
 import "log"
 
+// Level represents a specific log level.
+type Level int8
+
+const (
+	// LevelError is a log Level that should be used when logging runtime errors
+	// that are recoverable.
+	LevelError Level = iota
+	// LevelFatal is a log level that should be used when logging runtime errors
+	// that are not recoverable where the app terminates after logging.
+	LevelFatal
+)
+
+// Logger describes a type that can be used to log messages of different levels.
+type Logger interface{ Log(Level, string) }
+
 // AppLogger can be used to log messages of different log levels across the
 // project.
 type AppLogger struct{}
