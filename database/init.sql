2a110abd213b5cb27a8711fd2ea68d93e54ddb32
Atlas Kaplan <kxplxn@icloud.com>
2023-01-31 13:15:36 +0000

    #82 Postgres 10+ syntax is used for primary keys in init.sql

--- database/init.sql
+++ database/init.sql
@@ -4,32 +4,32 @@ CREATE TABLE IF NOT EXISTS app."user" (
 );
 
 CREATE TABLE IF NOT EXISTS app.user_board (
-  id      SERIAL      PRIMARY KEY,
+  id      INTEGER     PRIMARY KEY GENERATED ALWAYS      AS IDENTITY,
   userID  VARCHAR(15) NOT NULL    REFERENCES app."user",
   boardID SERIAL      NOT NULL    REFERENCES app.board,
   isAdmin BOOLEAN     NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS app.board (
-  id   SERIAL      PRIMARY KEY,
+  id   INTEGER     PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS app."column" (
-  id      SERIAL   PRIMARY KEY,
+  id      INTEGER  PRIMARY KEY GENERATED ALWAYS     AS IDENTITY,
   tableID SERIAL   NOT NULL    REFERENCES app.board,
   "order" SMALLINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS app.task (
-  id          SERIAL      PRIMARY KEY,
+  id          INTEGER     PRIMARY KEY GENERATED ALWAYS        AS IDENTITY,
   columnID    SERIAL      NOT NULL    REFERENCES app."column",
   title       VARCHAR(50) NOT NULL,
   description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS app.subtask (
-  id     SERIAL      PRIMARY KEY,
+  id     INTEGER     PRIMARY KEY GENERATED ALWAYS    AS IDENTITY,
   taskID SERIAL      NOT NULL    REFERENCES app.task,
   title  VARCHAR(50) NOT NULL,
   isDone BOOLEAN     NOT NULL
