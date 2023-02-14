6694050c97fa258a165b8c73a486b52bd1c2617e
Atlas Kaplan <kxplxn@icloud.com>
2023-02-14 09:24:29 +0000

    #88 Success case is added to /register integration tests; Makefile is fixed.

--- server/itest/register_test.go
+++ server/itest/register_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // TestRegister tests the /register route to assert that it behaves correctly
@@ -33,12 +34,12 @@ func TestRegister(t *testing.T) {
 	}
 
 	t.Run("ValidationErrs", func(t *testing.T) {
-		t.Log("dbConnStr: " + dbConnStr)
-
 		db, err := sql.Open("postgres", dbConnStr)
 		if err != nil {
 			t.Fatal(err.Error())
 		}
+
+		// insert a user into the database for the UsnTaken test case
 		existingUserID := "bob123"
 		_, err = db.Exec(
 			`INSERT INTO app."user"(id, password) VALUES ($1, $2)`,
@@ -49,30 +50,33 @@ func TestRegister(t *testing.T) {
 		}
 
 		for _, c := range []struct {
-			name         string
-			username     string
-			password     string
-			usernameErrs []string
-			passwordErrs []string
+			name             string
+			username         string
+			password         string
+			wantStatusCode   int
+			wantUsernameErrs []string
+			wantPasswordErrs []string
 		}{
 			{
-				name:         "UsnEmpty,PwdEmpty",
-				username:     "",
-				password:     "",
-				usernameErrs: []string{"Username cannot be empty."},
-				passwordErrs: []string{"Password cannot be empty."},
+				name:             "UsnEmpty,PwdEmpty",
+				username:         "",
+				password:         "",
+				wantStatusCode:   http.StatusBadRequest,
+				wantUsernameErrs: []string{"Username cannot be empty."},
+				wantPasswordErrs: []string{"Password cannot be empty."},
 			},
 			{
 				name: "UsnTooShort,UsnInvalidChar,PwdTooShort,PwdNoLower," +
 					"PwdNoDigit,PwdNoSpecial",
-				username: "bob!",
-				password: "PASSSSS",
-				usernameErrs: []string{
+				username:       "bob!",
+				password:       "PASSSSS",
+				wantStatusCode: http.StatusBadRequest,
+				wantUsernameErrs: []string{
 					"Username cannot be shorter than 5 characters.",
 					"Username can contain only letters (a-z/A-Z) and digits " +
 						"(0-9).",
 				},
-				passwordErrs: []string{
+				wantPasswordErrs: []string{
 					"Password cannot be shorter than 8 characters.",
 					"Password must contain a lowercase letter (a-z).",
 					"Password must contain a digit (0-9).",
@@ -87,11 +91,12 @@ func TestRegister(t *testing.T) {
 				username: "1bobobobobobobobo",
 				password: "p£$ 123p£$ 123p£$ 123p£$ 123p£$ 123p£$ 123p" +
 					"£$ 123p£$ 123p£$ 123p£",
-				usernameErrs: []string{
+				wantStatusCode: http.StatusBadRequest,
+				wantUsernameErrs: []string{
 					"Username cannot be longer than 15 characters.",
 					"Username can start only with a letter (a-z/A-Z).",
 				},
-				passwordErrs: []string{
+				wantPasswordErrs: []string{
 					"Password cannot be longer than 64 characters.",
 					"Password must contain an uppercase letter (A-Z).",
 					"Password cannot contain spaces.",
@@ -102,11 +107,20 @@ func TestRegister(t *testing.T) {
 				},
 			},
 			{
-				name:         "UsnTaken",
-				username:     existingUserID,
-				password:     "Myp4ssw0rd!",
-				usernameErrs: []string{"Username is already taken."},
-				passwordErrs: []string{},
+				name:             "UsnTaken",
+				username:         existingUserID,
+				password:         "Myp4ssw0rd!",
+				wantStatusCode:   http.StatusBadRequest,
+				wantUsernameErrs: []string{"Username is already taken."},
+				wantPasswordErrs: []string{},
+			},
+			{
+				name:             "Success",
+				username:         "bob321",
+				password:         "Myp4ssw0rd!",
+				wantStatusCode:   http.StatusOK,
+				wantUsernameErrs: []string{},
+				wantPasswordErrs: []string{},
 			},
 		} {
 			t.Run(c.name, func(t *testing.T) {
@@ -124,31 +138,49 @@ func TestRegister(t *testing.T) {
 					t.Fatal(err)
 				}
 
-				if err := assertEqual(
-					http.StatusBadRequest, res.StatusCode,
+				if err = assertEqual(
+					c.wantStatusCode, res.StatusCode,
 				); err != nil {
 					t.Error(err)
 				}
 
-				var resBody ResBody
-				if err := json.NewDecoder(res.Body).Decode(
-					&resBody,
-				); err != nil {
-					t.Fatal(err)
-				}
-
-				if err := assertEqualArr(
-					c.usernameErrs,
-					resBody.Errs.Username,
-				); err != nil {
-					t.Error(err)
-				}
-
-				if err := assertEqualArr(
-					c.passwordErrs,
-					resBody.Errs.Password,
-				); err != nil {
-					t.Error(err)
+				switch c.wantStatusCode {
+				case http.StatusOK:
+					var userID, password string
+					err = db.QueryRow(
+						`SELECT id, password FROM app."user" WHERE id = $1`,
+						c.username,
+					).Scan(&userID, &password)
+					if err != nil {
+						t.Fatal(err)
+					}
+					if err = assertEqual(c.username, userID); err != nil {
+						t.Error(err)
+					}
+					if err = bcrypt.CompareHashAndPassword(
+						[]byte(password), []byte(c.password),
+					); err != nil {
+						t.Error(err)
+					}
+				case http.StatusBadRequest:
+					var resBody ResBody
+					if err := json.NewDecoder(res.Body).Decode(
+						&resBody,
+					); err != nil {
+						t.Fatal(err)
+					}
+					if err := assertEqualArr(
+						c.wantUsernameErrs,
+						resBody.Errs.Username,
+					); err != nil {
+						t.Error(err)
+					}
+					if err := assertEqualArr(
+						c.wantPasswordErrs,
+						resBody.Errs.Password,
+					); err != nil {
+						t.Error(err)
+					}
 				}
 			})
 		}
