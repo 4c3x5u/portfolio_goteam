a49730de1648850d4b0486a74bc64f935a751fd0
Atlas Kaplan <kxplxn@icloud.com>
2023-02-14 16:00:47 +0000

    #88 Package itest package comment is updated.

--- server/itest/itest.go
+++ server/itest/itest.go
@@ -1,6 +1,6 @@
 //go:build itest
 
-// Package itest contains integration tests for the application. This module is
-// intended to be run independently of any actual project code. Therefore, some
-// code from the other modules are copied over for convenience.
+// Package itest contains integration tests for the application. Each Go file
+// except this one and main_test.go corresponds to a HTTP Handler used by the
+// application to serve an API endpoint.
 package itest
