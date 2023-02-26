78377f7852f46ca228460a2f96d81c33d3046c29
Atlas Kaplan <kxplxn@icloud.com>
2023-02-26 17:07:49 +0000

    #92 InvalidAuthToken case is added to board integration tests.

--- server/itest/board_test.go
+++ server/itest/board_test.go
@@ -39,38 +39,54 @@ func TestBoard(t *testing.T) {
 		},
 	)
 
-	t.Run("NoAuthHeader", func(t *testing.T) {
-		reqBody, err := json.Marshal(boardAPI.POSTReqBody{
-			Name: "New Board",
+	for _, c := range []struct {
+		name       string
+		authHeader string
+	}{
+		{name: "NoAuthHeader", authHeader: ""},
+		{
+			name: "InvalidAuthHeader",
+			authHeader: "eyJhbGciOiJIUzI1NiNowAsEtqKSQauaqow1.eyJzdWIiOiJib2I" +
+				"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			reqBody, err := json.Marshal(boardAPI.POSTReqBody{
+				Name: "New Board",
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			req, err := http.NewRequest(
+				http.MethodPost, "", bytes.NewReader(reqBody),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if c.authHeader != "" {
+				req.Header.Add("Authorization", "Bearer "+c.authHeader)
+			}
+			w := httptest.NewRecorder()
+
+			sut.ServeHTTP(w, req)
+
+			res := w.Result()
+
+			if err = assert.Equal(
+				http.StatusUnauthorized, res.StatusCode,
+			); err != nil {
+				t.Error(err)
+			}
+
+			wantAuthHeaderName, wantAuthHeaderValue := auth.WWWAuthenticate()
+			gotAuthHeaderValue := res.Header.Values(wantAuthHeaderName)[0]
+			if err := assert.Equal(
+				wantAuthHeaderValue,
+				gotAuthHeaderValue,
+			); err != nil {
+				t.Error(err)
+			}
+
 		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		req, err := http.NewRequest(
-			http.MethodPost, "", bytes.NewReader(reqBody),
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-		w := httptest.NewRecorder()
-
-		sut.ServeHTTP(w, req)
-
-		res := w.Result()
-
-		if err = assert.Equal(
-			http.StatusUnauthorized, res.StatusCode,
-		); err != nil {
-			t.Error(err)
-		}
-
-		wantAuthHeaderName, wantAuthHeaderValue := auth.WWWAuthenticate()
-		gotAuthHeaderValue := res.Header.Values(wantAuthHeaderName)[0]
-		if err := assert.Equal(
-			wantAuthHeaderValue,
-			gotAuthHeaderValue,
-		); err != nil {
-			t.Error(err)
-		}
-	})
+	}
 }
