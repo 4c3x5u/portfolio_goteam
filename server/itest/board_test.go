dd208bfbf7fa2181ecf8550970e105ba3061084e
Atlas Kaplan <kxplxn@icloud.com>
2023-03-04 16:51:01 +0000

    #92 Auth cases are separated from POST cases, and auth/assert funcs are used for case-specific auth/assertions in board integration tests.

--- server/itest/board_test.go
+++ server/itest/board_test.go
@@ -39,133 +39,165 @@ func TestBoard(t *testing.T) {
 		},
 	)
 
-	for _, c := range []struct {
-		name           string
-		authHeader     string
-		boardName      string
-		wantStatusCode int
-		wantErrMsg     string
-		userID         string // only used for board count on POST success
-	}{
-		// Auth Cases
-		{
-			name:           "NoAuthHeader",
-			authHeader:     "",
-			boardName:      "",
-			wantStatusCode: http.StatusUnauthorized,
-			wantErrMsg:     "",
-			userID:         "",
-		},
-		{
-			name: "InvalidAuthHeader",
-			authHeader: "eyJhbGciOiJIUzI1NiNowAsEtqKSQauaqow1.eyJzdWIiOiJib2I" +
-				"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
-			boardName:      "",
-			wantStatusCode: http.StatusUnauthorized,
-			wantErrMsg:     "",
-			userID:         "",
-		},
+	// used in varioues test cases to authenticate the request sent
+	addBearerAuth := func(token string) func(*http.Request) {
+		return func(req *http.Request) {
+			req.Header.Add("Authorization", "Bearer "+token)
+		}
+	}
 
-		// POST Cases
-		{
-			name: "EmptyBoardName",
-			authHeader: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
-				"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
-			boardName:      "",
-			wantStatusCode: http.StatusBadRequest,
-			wantErrMsg:     "Board name cannot be empty.",
-			userID:         "",
-		},
-		{
-			name: "TooLongBoardName",
-			authHeader: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
-				"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
-			boardName:      "A Board Whose Name Is Just Too Long!",
-			wantStatusCode: http.StatusBadRequest,
-			wantErrMsg:     "Board name cannot be longer than 35 characters.",
-			userID:         "",
-		},
-		{
-			name: "TooManyBoards",
-			authHeader: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
-				"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
-			boardName:      "bob123's new board",
-			wantStatusCode: http.StatusBadRequest,
-			wantErrMsg: "You have already created the maximum amount of " +
-				"boards allowed per user. Please delete one of your boards " +
-				"to create a new one.",
-			userID: "",
-		},
-		{
-			name: "Succeess",
-			authHeader: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
-				"xMjQifQ.LqENrj9APUHgQ3X0HRN6-IFMIg6nyo0_n74KfoxA0qI",
-			boardName:      "bob124's new board",
-			wantStatusCode: http.StatusOK,
-			wantErrMsg:     "",
-			userID:         "bob124",
-		},
-	} {
-		t.Run(c.name, func(t *testing.T) {
-			reqBody, err := json.Marshal(boardAPI.POSTReqBody{
-				Name: c.boardName,
-			})
-			if err != nil {
-				t.Fatal(err)
-			}
-			req, err := http.NewRequest(
-				http.MethodPost, "", bytes.NewReader(reqBody),
-			)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if c.authHeader != "" {
-				req.Header.Add("Authorization", "Bearer "+c.authHeader)
-			}
-			w := httptest.NewRecorder()
-
-			sut.ServeHTTP(w, req)
-
-			res := w.Result()
-
-			if err = assert.Equal(
-				c.wantStatusCode, res.StatusCode,
+	// used in 400 error cases to assert on the error message
+	assertOnErrMsg := func(
+		wantErrMsg string,
+	) func(*testing.T, *httptest.ResponseRecorder) {
+		return func(t *testing.T, w *httptest.ResponseRecorder) {
+			resBody := boardAPI.POSTResBody{}
+			if err := json.NewDecoder(w.Result().Body).Decode(
+				&resBody,
 			); err != nil {
 				t.Error(err)
 			}
-
-			switch c.wantStatusCode {
-			case http.StatusUnauthorized:
-				authResHeader := res.Header.Values("WWW-Authenticate")[0]
-				if err = assert.Equal("Bearer", authResHeader); err != nil {
-					t.Error(err)
-				}
-			case http.StatusBadRequest:
-				resBody := boardAPI.POSTResBody{}
-				if err = json.NewDecoder(w.Result().Body).Decode(
-					&resBody,
-				); err != nil {
-					t.Error(err)
-				}
-				if err = assert.Equal(
-					c.wantErrMsg, resBody.Error,
-				); err != nil {
-					t.Error(err)
-				}
-			case http.StatusOK:
-				var boardCount int
-				err = dbConnPool.QueryRow(
-					"SELECT COUNT(*) FROM app.user_board "+
-						"WHERE userID = $1 AND isAdmin = TRUE",
-					c.userID,
-				).Scan(&boardCount)
-				if err != nil {
-					t.Error(err)
-				}
-				if err = assert.Equal(1, boardCount); err != nil {
-					t.Error(err)
-				}
+			if err := assert.Equal(
+				wantErrMsg, resBody.Error,
+			); err != nil {
+				t.Error(err)
 			}
-		})
+		}
 	}
+
+	t.Run("Auth", func(t *testing.T) {
+		for _, c := range []struct {
+			name     string
+			authFunc func(*http.Request)
+		}{
+			// Auth Cases
+			{name: "HeaderEmpty", authFunc: func(_ *http.Request) {}},
+			{name: "HeaderInvalid", authFunc: addBearerAuth("asdfasldfkjasd")},
+		} {
+			t.Run(c.name, func(t *testing.T) {
+				for _, method := range []string{
+					http.MethodPost, http.MethodDelete,
+				} {
+					t.Run(method, func(t *testing.T) {
+						req, err := http.NewRequest(method, "", nil)
+						if err != nil {
+							t.Fatal(err)
+						}
+						c.authFunc(req)
+						w := httptest.NewRecorder()
+
+						sut.ServeHTTP(w, req)
+						res := w.Result()
+
+						if err = assert.Equal(
+							http.StatusUnauthorized, res.StatusCode,
+						); err != nil {
+							t.Error(err)
+						}
+
+						if err = assert.Equal(
+							"Bearer", res.Header.Values("WWW-Authenticate")[0],
+						); err != nil {
+							t.Error(err)
+						}
+					})
+				}
+			})
+		}
+	})
+
+	t.Run("POST", func(t *testing.T) {
+		for _, c := range []struct {
+			name           string
+			authFunc       func(*http.Request)
+			boardName      string
+			wantStatusCode int
+			assertFunc     func(*testing.T, *httptest.ResponseRecorder)
+		}{
+			{
+				name: "EmptyBoardName",
+				authFunc: addBearerAuth(
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
+						"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
+				),
+				boardName:      "",
+				wantStatusCode: http.StatusBadRequest,
+				assertFunc:     assertOnErrMsg("Board name cannot be empty."),
+			},
+			{
+				name: "TooLongBoardName",
+				authFunc: addBearerAuth(
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
+						"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
+				),
+				boardName:      "A Board Whose Name Is Just Too Long!",
+				wantStatusCode: http.StatusBadRequest,
+				assertFunc: assertOnErrMsg(
+					"Board name cannot be longer than 35 characters.",
+				)},
+			{
+				name: "TooManyBoards",
+				authFunc: addBearerAuth(
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
+						"xMjMifQ.Y8_6K50EHUEJlJf4X21fNCFhYWhVIqN3Tw1niz8XwZc",
+				),
+				boardName:      "bob123's new board",
+				wantStatusCode: http.StatusBadRequest,
+				assertFunc: assertOnErrMsg(
+					"You have already created the maximum amount of boards " +
+						"allowed per user. Please delete one of your boards " +
+						"to create a new one.",
+				)},
+			{
+				name: "Success",
+				authFunc: addBearerAuth(
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2I" +
+						"xMjQifQ.LqENrj9APUHgQ3X0HRN6-IFMIg6nyo0_n74KfoxA0qI",
+				),
+				boardName:      "bob124's new board",
+				wantStatusCode: http.StatusOK,
+				assertFunc: func(_ *testing.T, _ *httptest.ResponseRecorder) {
+					var boardCount int
+					err := dbConnPool.QueryRow(
+						"SELECT COUNT(*) FROM app.user_board " +
+							"WHERE userID = 'bob124' AND isAdmin = TRUE",
+					).Scan(&boardCount)
+					if err != nil {
+						t.Error(err)
+					}
+					if err = assert.Equal(1, boardCount); err != nil {
+						t.Error(err)
+					}
+				},
+			},
+		} {
+			t.Run(c.name, func(t *testing.T) {
+				reqBodyBytes, err := json.Marshal(boardAPI.POSTReqBody{
+					Name: c.boardName,
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+				req, err := http.NewRequest(
+					http.MethodPost, "", bytes.NewReader(reqBodyBytes),
+				)
+				if err != nil {
+					t.Fatal(err)
+				}
+				c.authFunc(req)
+				w := httptest.NewRecorder()
+
+				sut.ServeHTTP(w, req)
+				res := w.Result()
+
+				if err = assert.Equal(
+					c.wantStatusCode, res.StatusCode,
+				); err != nil {
+					t.Error(err)
+				}
+
+				c.assertFunc(t, w)
+			})
+		}
+	})
 }
