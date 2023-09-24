46cd84691c6ab54226bf12ad1848cea7b26969aa
Atlas Kaplan <62238518+kxplxn@users.noreply.github.com>
2023-09-24 02:50:11 +0100

    #102 The redundant fakeIDValidator is removed from the fakes file.

--- server/api/board/fake.go
+++ server/api/board/fake.go
@@ -5,10 +5,3 @@ type fakeStringValidator struct{ OutErr error }
 
 // Validate implements the StringValidator interface on fakeStringValidator.
 func (f *fakeStringValidator) Validate(_ string) error { return f.OutErr }
-
-// fakeIDValidator is a test fake for IDValidator.
-type fakeIDValidator struct{ OutErr error }
-
-// Validate implements the DELETEReqValidator interface on
-// fakeIDValidator.
-func (f *fakeIDValidator) Validate(_ string) error { return f.OutErr }
