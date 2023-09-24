f7c0c3a91fb5a3608200738ae452ee422616c638
Atlas Kaplan <62238518+kxplxn@users.noreply.github.com>
2023-09-24 03:03:18 +0100

    #102 Patch handler tests are refactored into table-driven format.

--- server/api/board/patchHandler_test.go
+++ server/api/board/patchHandler_test.go
@@ -13,82 +13,62 @@ import (
 )
 
 func TestPATCHHandler(t *testing.T) {
-	t.Run("IDValidatorErr", func(t *testing.T) {
-		log := &pkgLog.FakeErrorer{}
-		idValidator := &fakeStringValidator{}
-		nameValidator := &fakeStringValidator{}
-		sut := NewPATCHHandler(idValidator, nameValidator, log)
+	log := &pkgLog.FakeErrorer{}
+	idValidator := &fakeStringValidator{}
+	nameValidator := &fakeStringValidator{}
+	sut := NewPATCHHandler(idValidator, nameValidator, log)
 
-		wantErrMsg := "Board ID cannot be empty."
-		wantStatusCode := http.StatusBadRequest
+	for _, c := range []struct {
+		name                string
+		idValidatorOutErr   error
+		nameValidatorOutErr error
+		wantErrMsg          string
+	}{
+		{
+			name:                "IDValidatorErr",
+			idValidatorOutErr:   errors.New("Board ID cannot be empty."),
+			nameValidatorOutErr: nil,
+			wantErrMsg:          "Board ID cannot be empty.",
+		},
+		{
+			name:                "NameValidatorErr",
+			idValidatorOutErr:   nil,
+			nameValidatorOutErr: errors.New("Board name cannot be empty."),
+			wantErrMsg:          "Board name cannot be empty.",
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			idValidator.OutErr = c.idValidatorOutErr
+			nameValidator.OutErr = c.nameValidatorOutErr
 
-		idValidator.OutErr = errors.New(wantErrMsg)
+			reqBody, err := json.Marshal(ReqBody{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			req, err := http.NewRequest(
+				http.MethodPatch, "", bytes.NewReader(reqBody),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			w := httptest.NewRecorder()
 
-		reqBody, err := json.Marshal(ReqBody{})
-		if err != nil {
-			t.Fatal(err)
-		}
-		req, err := http.NewRequest(
-			http.MethodPatch, "", bytes.NewReader(reqBody),
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-		w := httptest.NewRecorder()
+			sut.Handle(w, req, "")
+			res := w.Result()
 
-		sut.Handle(w, req, "")
-		res := w.Result()
+			if err = assert.Equal(
+				http.StatusBadRequest, res.StatusCode,
+			); err != nil {
+				t.Error(err)
+			}
 
-		if err = assert.Equal(wantStatusCode, res.StatusCode); err != nil {
-			t.Error(err)
-		}
-
-		var resBody ResBody
-		if err = json.NewDecoder(res.Body).Decode(&resBody); err != nil {
-			t.Fatal(err)
-		}
-		if err = assert.Equal(wantErrMsg, resBody.Error); err != nil {
-			t.Error(err)
-		}
-	})
-
-	t.Run("NameValidatorErr", func(t *testing.T) {
-		log := &pkgLog.FakeErrorer{}
-		idValidator := &fakeStringValidator{}
-		nameValidator := &fakeStringValidator{}
-		sut := NewPATCHHandler(idValidator, nameValidator, log)
-
-		wantErrMsg := "Board name cannot be empty."
-		wantStatusCode := http.StatusBadRequest
-
-		nameValidator.OutErr = errors.New(wantErrMsg)
-
-		reqBody, err := json.Marshal(ReqBody{})
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		req, err := http.NewRequest(
-			http.MethodPatch, "", bytes.NewReader(reqBody),
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-		w := httptest.NewRecorder()
-
-		sut.Handle(w, req, "")
-		res := w.Result()
-
-		if err = assert.Equal(wantStatusCode, res.StatusCode); err != nil {
-			t.Error(err)
-		}
-
-		var resBody ResBody
-		if err = json.NewDecoder(res.Body).Decode(&resBody); err != nil {
-			t.Fatal(err)
-		}
-		if err = assert.Equal(wantErrMsg, resBody.Error); err != nil {
-			t.Error(err)
-		}
-	})
+			var resBody ResBody
+			if err = json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+				t.Fatal(err)
+			}
+			if err = assert.Equal(c.wantErrMsg, resBody.Error); err != nil {
+				t.Error(err)
+			}
+		})
+	}
 }
