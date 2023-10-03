8404d060e8b4da9d8e7a1160b8901ec9263a81b5
Atlas Kaplan <62238518+kxplxn@users.noreply.github.com>
2023-10-03 10:00:33 +0100

    Package dbaccess' setUpDBTest function's teardown return is assigned to a variable to clarify intent.

--- server/dbaccess/mock.go
+++ server/dbaccess/mock.go
@@ -7,18 +7,18 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
-// setUpDBTest sets up the DB and Sqlmock for server/db test cases. The
-// first two return values are self-explanatory, and the third is the database
-// teardown function to be deferred by the test case.
+// setUpDBTest sets up the *sql.DB and *sqlmock.Sqlmock for server/db test
+// cases.
 func setUpDBTest(t *testing.T) (*sql.DB, sqlmock.Sqlmock, func()) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatal(err)
 	}
-	return db, mock, func() {
+	teardown := func() {
 		mock.ExpectClose()
 		if err = db.Close(); err != nil {
 			t.Fatal(err)
 		}
 	}
+	return db, mock, teardown
 }
