bd6a19e5c298935bce4d4d1d71bd433bc42fff3f
Atlas Kaplan <62238518+kxplxn@users.noreply.github.com>
2023-10-05 16:07:03 +0100

    #112 Updater tests are made table-driven.

--- server/dbaccess/column/updater_test.go
+++ server/dbaccess/column/updater_test.go
@@ -15,9 +15,9 @@ import (
 // returns whatever error occurs.
 func TestUpdater(t *testing.T) {
 	var (
-		columnID = "1"
-		tasks    = []Task{{ID: 1, Order: 1}, {ID: 2, Order: 2}, {ID: 3, Order: 3}}
-		wantErr  = sql.ErrNoRows
+		columnID      = "1"
+		tasks         = []Task{{ID: 1, Order: 1}, {ID: 2, Order: 2}, {ID: 3, Order: 3}}
+		sqlUpdateTask = `UPDATE app.task SET columnID = \$1 AND order = \$2 WHERE id = \$3`
 	)
 
 	db, mock, teardown := dbaccess.SetUpDBTest(t)
@@ -25,25 +25,38 @@ func TestUpdater(t *testing.T) {
 
 	sut := NewUpdater(db)
 
-	mock.ExpectExec(
-		`UPDATE app.task SET columnID = \$1 AND order = \$2 WHERE id = \$3`,
-	).WithArgs(columnID, tasks[0].Order, tasks[0].ID).
-		WillReturnError(wantErr)
-
-	for _, task := range tasks {
-		mock.ExpectExec(
-			`UPDATE app.task SET columnID = \$1 AND order = \$2 WHERE id = \$3`,
-		).WithArgs(columnID, task.Order, task.ID).
-			WillReturnResult(sqlmock.NewResult(0, 1))
-	}
-
-	err := sut.Update(columnID, tasks)
-	if assertErr := assert.SameError(wantErr, err); assertErr != nil {
-		t.Error(assertErr)
-	}
-
-	err = sut.Update(columnID, tasks)
-	if assertErr := assert.Nil(err); assertErr != nil {
-		t.Error(assertErr)
+	for _, c := range []struct {
+		name     string
+		mockFunc func(sqlmock.Sqlmock)
+		wantErr  error
+	}{
+		{
+			name: "Error",
+			mockFunc: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(sqlUpdateTask).
+					WithArgs(columnID, tasks[0].Order, tasks[0].ID).
+					WillReturnError(sql.ErrNoRows)
+			},
+			wantErr: sql.ErrNoRows,
+		},
+		{
+			name: "Success",
+			mockFunc: func(mock sqlmock.Sqlmock) {
+				for _, task := range tasks {
+					mock.ExpectExec(sqlUpdateTask).
+						WithArgs(columnID, task.Order, task.ID).
+						WillReturnResult(sqlmock.NewResult(0, 1))
+				}
+			},
+			wantErr: nil,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			c.mockFunc(mock)
+			err := sut.Update(columnID, tasks)
+			if assertErr := assert.SameError(c.wantErr, err); assertErr != nil {
+				t.Error(assertErr)
+			}
+		})
 	}
 }
