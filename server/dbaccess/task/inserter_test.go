ffb02bc8db9057576b668fa5ecc6b2ed648a3b55
Atlas Kaplan <62238518+kxplxn@users.noreply.github.com>
2023-10-08 08:52:46 +0100

    #113 Inserter tests are made table-driven to reduce duplication.

--- server/dbaccess/task/inserter_test.go
+++ server/dbaccess/task/inserter_test.go
@@ -17,56 +17,61 @@ import (
 // whatever error occurs.
 func TestInserter(t *testing.T) {
 	task := NewTask(2, "Some Task", "Do something.")
-	wantErr := errors.New("an error occurred")
+	anErr := errors.New("an error occurred")
 
 	db, mock, teardown := dbaccess.SetUpDBTest(t)
 	defer teardown()
 
 	sut := NewInserter(db)
 
-	t.Run("BeginErr", func(t *testing.T) {
-		mock.ExpectBegin().WillReturnError(wantErr)
-
+	for _, c := range []struct {
+		name      string
+		setUpMock func(sqlmock.Sqlmock)
+		wantErr   error
+	}{
+		{
+			name: "BeginErr",
+			setUpMock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin().WillReturnError(anErr)
+			},
+			wantErr: anErr,
+		},
+		{
+			name: "SelectOrderErr",
+			setUpMock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectQuery(
+					`SELECT "order" FROM app.task WHERE columnID = \$1 ` +
+						`ORDER BY "order" DESC LIMIT 1`,
+				).WithArgs(task.columnID).WillReturnError(anErr)
+			},
+			wantErr: anErr,
+		},
+		{
+			name: "InsertTaskErr",
+			setUpMock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectQuery(
+					`SELECT "order" FROM app.task WHERE columnID = \$1 ` +
+						`ORDER BY "order" DESC LIMIT 1`,
+				).WithArgs(task.columnID).WillReturnRows(
+					sqlmock.NewRows([]string{"order"}).AddRow(5),
+				)
+				mock.ExpectExec(
+					`INSERT INTO app.task`+
+						`\(columnID, title, description, \"order\"\)`+
+						`VALUES \(\$1, \$2, \$3, \$4\)`,
+				).WithArgs(
+					task.columnID, task.title, task.description, 6,
+				).WillReturnError(anErr)
+			},
+			wantErr: anErr,
+		},
+	} {
+		c.setUpMock(mock)
 		err := sut.Insert(task)
-
-		if assertErr := assert.SameError(wantErr, err); assertErr != nil {
+		if assertErr := assert.SameError(c.wantErr, err); assertErr != nil {
 			t.Error(assertErr)
 		}
-	})
-
-	t.Run("GetHighestOrderErr", func(t *testing.T) {
-		mock.ExpectBegin()
-		mock.ExpectQuery(
-			`SELECT "order" FROM app.task WHERE columnID = \$1 ` +
-				`ORDER BY "order" DESC LIMIT 1`,
-		).WithArgs(task.columnID).WillReturnError(wantErr)
-
-		err := sut.Insert(task)
-
-		if assertErr := assert.SameError(wantErr, err); assertErr != nil {
-			t.Error(assertErr)
-		}
-	})
-
-	t.Run("InsertTaskErr", func(t *testing.T) {
-		mock.ExpectBegin()
-		mock.ExpectQuery(
-			`SELECT "order" FROM app.task WHERE columnID = \$1 ` +
-				`ORDER BY "order" DESC LIMIT 1`,
-		).WithArgs(task.columnID).WillReturnRows(
-			sqlmock.NewRows([]string{"order"}).AddRow(5),
-		)
-		mock.ExpectExec(
-			`INSERT INTO app.task\(columnID, title, description, \"order\"\)`+
-				`VALUES \(\$1, \$2, \$3, \$4\)`,
-		).WithArgs(
-			task.columnID, task.title, task.description, 6,
-		).WillReturnError(wantErr)
-
-		err := sut.Insert(task)
-
-		if assertErr := assert.SameError(wantErr, err); assertErr != nil {
-			t.Error(assertErr)
-		}
-	})
+	}
 }
