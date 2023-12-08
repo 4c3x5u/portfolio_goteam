530a1228191e274b0d063cba0035be5ff2febd16
Atlas Kaplan <aakaplan@proton.me>
2023-12-08 08:39:49 +0000

    #126 remove now-redundant auth.Generator

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,13 +1,8 @@
 // Package auth contains code that handles authentication-related concerns.
 package auth
 
-import "time"
-
 // CookieName is the name of the cookie that the auth token is stored in.
 const CookieName = "auth-token"
-
-// Duration is the amount of time that an auth token/session should last.
-const Duration = 1 * time.Hour
 
 // WWWAuthenticate is a helper function that returns the name and the value of
 // the WWW-Authenticate header that must be set when returning a 401
@@ -15,7 +10,3 @@ const Duration = 1 * time.Hour
 func WWWAuthenticate() (string, string) {
 	return "WWW-Authenticate", "Bearer"
 }
-
-// AuthorizationHeader is the name of the header that is used to retrieve the
-// authentication token from a HTTP request.
-const AuthorizationHeader = "Authorization"
