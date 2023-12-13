eb6e1c421d85520ba752185088979c53fd1c616d
Atlas Kaplan <aakaplan@proton.me>
2023-12-13 10:06:49 +0000

    #126 team integration test refactor, check db on created

--- test/api/team_test.go
+++ test/api/team_test.go
@@ -3,10 +3,15 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 
 	teamAPI "github.com/kxplxn/goteam/internal/api/team"
 	"github.com/kxplxn/goteam/pkg/assert"
@@ -24,115 +29,135 @@ func TestTeamAPI(t *testing.T) {
 	)
 
 	t.Run("GET", func(t *testing.T) {
-		t.Run("NoAuth", func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodGet, "/team", nil)
-			w := httptest.NewRecorder()
-			handler.Handle(w, r, "")
+		for _, c := range []struct {
+			name       string
+			authFunc   func(*http.Request)
+			wantStatus int
+			assertFunc func(*testing.T, *http.Response)
+		}{
+			{
+				name:       "NoAuth",
+				authFunc:   func(r *http.Request) {},
+				wantStatus: http.StatusUnauthorized,
+				assertFunc: func(*testing.T, *http.Response) {},
+			},
+			{
+				name:       "InvalidAuth",
+				authFunc:   addCookieAuth("asdfasdf"),
+				wantStatus: http.StatusUnauthorized,
+				assertFunc: func(*testing.T, *http.Response) {},
+			},
+			{
+				name:       "OK",
+				authFunc:   addCookieAuth(tkTeam1Member),
+				wantStatus: http.StatusOK,
+				assertFunc: func(t *testing.T, r *http.Response) {
+					wantResp := teamAPI.GetResp{
+						ID:      "afeadc4a-68b0-4c33-9e83-4648d20ff26a",
+						Members: []string{"team1Admin", "team1Member"},
+						Boards: []teamTable.Board{
+							{
+								ID:   "91536664-9749-4dbb-a470-6e52aa353ae4",
+								Name: "Team 1 Board 1",
+							},
+							{
+								ID:   "fdb82637-f6a5-4d55-9dc3-9f60061e632f",
+								Name: "Team 1 Board 2",
+							},
+							{
+								ID:   "1559a33c-54c5-42c8-8e5f-fe096f7760fa",
+								Name: "Team 1 Board 3",
+							},
+						},
+					}
 
-			res := w.Result()
+					var resp teamAPI.GetResp
+					err := json.NewDecoder(r.Body).Decode(&resp)
+					if err != nil {
+						t.Fatal(err)
+					}
 
-			assert.Equal(t.Error, res.StatusCode, http.StatusUnauthorized)
-		})
-
-		t.Run("InvalidAuth", func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodGet, "/team", nil)
-			r.AddCookie(&http.Cookie{Name: "auth-token", Value: "asdfasdf"})
-			w := httptest.NewRecorder()
-			handler.Handle(w, r, "")
-
-			res := w.Result()
-
-			assert.Equal(t.Error, res.StatusCode, http.StatusUnauthorized)
-		})
-
-		t.Run("OK", func(t *testing.T) {
-			wantResp := teamAPI.GetResp{
-				ID:      "afeadc4a-68b0-4c33-9e83-4648d20ff26a",
-				Members: []string{"team1Admin", "team1Member"},
-				Boards: []teamTable.Board{
-					{
-						ID:   "91536664-9749-4dbb-a470-6e52aa353ae4",
-						Name: "Team 1 Board 1",
-					},
-					{
-						ID:   "fdb82637-f6a5-4d55-9dc3-9f60061e632f",
-						Name: "Team 1 Board 2",
-					},
-					{
-						ID:   "1559a33c-54c5-42c8-8e5f-fe096f7760fa",
-						Name: "Team 1 Board 3",
-					},
+					assert.Equal(t.Error, resp.ID, wantResp.ID)
+					assert.AllEqual(t.Error, resp.Members, wantResp.Members)
+					assert.Equal(t.Error, len(resp.Boards), len(wantResp.Boards))
+					for i, b := range wantResp.Boards {
+						assert.Equal(t.Error, resp.Boards[i].ID, b.ID)
+						assert.Equal(t.Error, resp.Boards[i].Name, b.Name)
+					}
 				},
-			}
-			r := httptest.NewRequest(http.MethodGet, "/team", nil)
-			addCookieAuth(tkTeam1Member)(r)
-			w := httptest.NewRecorder()
+			},
+			// refer to comments in implementation for the below tests
+			{
+				name: "NotAdmin",
+				authFunc: addCookieAuth(
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjpmYWx" +
+						"zZX0.Uz6JmqHbxSrzyKAIktxRW4Y_0ldqi_bEcNkYfvIIM8I",
+				),
+				wantStatus: http.StatusUnauthorized,
+				assertFunc: func(t *testing.T, r *http.Response) {},
+			},
+			{
+				name: "Created",
+				authFunc: addCookieAuth(
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cn" +
+						"VlLCJ0ZWFtSUQiOiJkNWRjYTliYy1iYzk4LTQ3YjQtYjhiNy05Z" +
+						"jAxODEzZGE1NzEiLCJ1c2VybmFtZSI6Im5ld3VzZXIifQ.lCjQi" +
+						"rzU_3yxOi2bNXRLuyxgzbUnEftITcIFMz2jCb8",
+				),
+				wantStatus: http.StatusCreated,
+				assertFunc: func(t *testing.T, r *http.Response) {
+					wantMembers := []string{"newuser"}
+					wantBoardLen := 1
+					wantBoardName := "New Board"
 
-			handler.Handle(w, r, "")
+					// assert on response
+					var resp teamAPI.GetResp
+					err := json.NewDecoder(r.Body).Decode(&resp)
+					if err != nil {
+						t.Fatal(err)
+					}
+					assert.AllEqual(t.Error, resp.Members, wantMembers)
+					assert.Equal(t.Error, len(resp.Boards), wantBoardLen)
+					assert.Equal(t.Error, resp.Boards[0].Name, wantBoardName)
 
-			res := w.Result()
+					// asssert on db
+					out, err := svcDynamo.GetItem(
+						context.Background(),
+						&dynamodb.GetItemInput{
+							TableName: &teamTableName,
+							Key: map[string]types.AttributeValue{
+								"ID": &types.AttributeValueMemberS{
+									Value: resp.ID,
+								},
+							},
+						},
+					)
+					if err != nil {
+						t.Fatal(err)
+					}
+					var team teamTable.Team
+					err = attributevalue.UnmarshalMap(out.Item, &team)
+					if err != nil {
+						t.Fatal(err)
+					}
+					assert.AllEqual(t.Error, team.Members, wantMembers)
+					assert.Equal(t.Error, len(team.Boards), wantBoardLen)
+					assert.Equal(t.Error, team.Boards[0].Name, wantBoardName)
+				},
+			},
+		} {
+			t.Run(c.name, func(t *testing.T) {
+				r := httptest.NewRequest(http.MethodGet, "/team", nil)
+				c.authFunc(r)
+				w := httptest.NewRecorder()
+				handler.Handle(w, r, "")
 
-			assert.Equal(t.Error, res.StatusCode, http.StatusOK)
+				res := w.Result()
 
-			var resp teamAPI.GetResp
-			if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
-				t.Fatal(err)
-			}
+				assert.Equal(t.Error, res.StatusCode, c.wantStatus)
 
-			assert.Equal(t.Error, resp.ID, wantResp.ID)
-			for i, m := range wantResp.Members {
-				assert.Equal(t.Error, resp.Members[i], m)
-			}
-			for i, b := range wantResp.Boards {
-				assert.Equal(t.Error, resp.Boards[i].ID, b.ID)
-				assert.Equal(t.Error, resp.Boards[i].Name, b.Name)
-			}
-		})
-	})
-
-	// if no team was found and since we trust the token, this is our sign from
-	// the user endpoint that we should create a new team for the user
-
-	// however, we should not create a new team if the user is not admin - this
-	// case should never happen but we should still test it
-	t.Run("NotAdmin", func(t *testing.T) {
-		authNotAdmin := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp" +
-			"mYWxzZX0.Uz6JmqHbxSrzyKAIktxRW4Y_0ldqi_bEcNkYfvIIM8I"
-		r := httptest.NewRequest(http.MethodGet, "/team", nil)
-		addCookieAuth(authNotAdmin)(r)
-		w := httptest.NewRecorder()
-
-		handler.Handle(w, r, "")
-
-		res := w.Result()
-		assert.Equal(t.Error, res.StatusCode, http.StatusUnauthorized)
-	})
-
-	t.Run("Created", func(t *testing.T) {
-		authAdmin := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjp0cn" +
-			"VlLCJ0ZWFtSUQiOiJkNWRjYTliYy1iYzk4LTQ3YjQtYjhiNy05ZjAxODEzZGE1Nz" +
-			"EiLCJ1c2VybmFtZSI6Im5ld3VzZXIifQ.lCjQirzU_3yxOi2bNXRLuyxgzbUnEft" +
-			"ITcIFMz2jCb8"
-		wantResp := teamAPI.GetResp{
-			ID:      "d5dca9bc-bc98-47b4-b8b7-9f01813da571",
-			Members: []string{"newuser"},
-			Boards:  []teamTable.Board{{Name: "New Board"}},
+				c.assertFunc(t, res)
+			})
 		}
-		r := httptest.NewRequest(http.MethodGet, "/team", nil)
-		addCookieAuth(authAdmin)(r)
-		w := httptest.NewRecorder()
-
-		handler.Handle(w, r, "")
-
-		res := w.Result()
-		var resp teamAPI.GetResp
-		if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
-			t.Fatal(err)
-		}
-
-		assert.Equal(t.Error, resp.ID, wantResp.ID)
-		assert.AllEqual(t.Error, resp.Members, wantResp.Members)
-		assert.Equal(t.Error, len(resp.Boards), 1)
-		assert.Equal(t.Error, resp.Boards[0].Name, wantResp.Boards[0].Name)
 	})
 }
