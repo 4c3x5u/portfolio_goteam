f13e042dad7f706cd65d9be023cbc3f76d8d9d98
Atlas Kaplan <aakaplan@proton.me>
2023-12-29 08:40:37 +0000

    #130 create validate POST task request function

--- internal/tasksvc/taskapi/validate.go
+++ internal/tasksvc/taskapi/validate.go
@@ -1,6 +1,10 @@
 package taskapi
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
+
 	"github.com/kxplxn/goteam/pkg/validator"
 )
 
@@ -34,3 +38,66 @@ func (v ColNoValidator) Validate(number int) error {
 	}
 	return nil
 }
+
+// ValidatePostReq validates a given PostReq.
+func ValidatePostReq(req PostReq) error {
+	if req.BoardID == "" {
+		return errBoardIDEmpty
+	}
+	if _, err := uuid.Parse(req.BoardID); err != nil {
+		return errParseBoardID
+	}
+	if req.ColNo < 0 || req.ColNo > 3 {
+		return errColNoOutOfBounds
+	}
+	if req.Title == "" {
+		return errTitleEmpty
+	}
+	if len(req.Title) > 50 {
+		return errTitleTooLong
+	}
+	if len(req.Description) > 500 {
+		return errDescriptionTooLong
+	}
+	for _, st := range req.Subtasks {
+		if st.Title == "" {
+			return errSubtaskTitleEmpty
+		}
+		if len(st.Title) > 50 {
+			return errSubtaskTitleTooLong
+		}
+	}
+	if req.Order < 0 {
+		return errOrderNegative
+	}
+	return nil
+}
+
+var (
+	// errBoardIDEmpty is returned when a board ID is empty.
+	errBoardIDEmpty = errors.New("board id is empty")
+
+	// errParseBoardID is returned when a board ID cannot be parsed.
+	errParseBoardID = errors.New("could not parse board id")
+
+	// errColNoOutOfBounds is returned when a column number is out of bounds.
+	errColNoOutOfBounds = errors.New("column number is out of bounds")
+
+	// errTitleEmpty is returned when a task title is empty.
+	errTitleEmpty = errors.New("title is empty")
+
+	// errTitleTooLong is returned when a task title is too long.
+	errTitleTooLong = errors.New("title is too long")
+
+	// errTitleEmpty is returned when a task description is too long.
+	errDescriptionTooLong = errors.New("description is too long")
+
+	// errSubtaskTitleEmpty is returned when a subtask is empty.
+	errSubtaskTitleEmpty = errors.New("subtask is empty")
+
+	// errSubtaskTitleTooLong is returned when a subtask is too long.
+	errSubtaskTitleTooLong = errors.New("subtask is too long")
+
+	// errOrderNegative is returned when the order is negative.
+	errOrderNegative = errors.New("order is negative")
+)
--- internal/tasksvc/taskapi/validate_test.go
+++ internal/tasksvc/taskapi/validate_test.go
@@ -6,8 +6,141 @@ import (
 	"testing"
 
 	"github.com/kxplxn/goteam/pkg/assert"
+	"github.com/kxplxn/goteam/pkg/db/tasktbl"
 	"github.com/kxplxn/goteam/pkg/validator"
 )
+
+// TestValidatePostReq tests the ValidatePostReq function to assert that it
+// returns the correct error based on the PostReq input.
+func TestValidatePostReq(t *testing.T) {
+	sut := ValidatePostReq
+
+	for _, c := range []struct {
+		name    string
+		req     PostReq
+		wantErr error
+	}{
+		{
+			name:    "BoardIDEmpty",
+			req:     PostReq{BoardID: ""},
+			wantErr: errBoardIDEmpty,
+		},
+		{
+			name:    "BoardIDInvalid",
+			req:     PostReq{BoardID: "invalid"},
+			wantErr: errParseBoardID,
+		},
+		{
+			name: "ColNoTooSmall",
+			req: PostReq{
+				BoardID: "00000000-0000-0000-0000-000000000000",
+				ColNo:   -1,
+			},
+			wantErr: errColNoOutOfBounds,
+		},
+		{
+			name: "ColNoTooBig",
+			req: PostReq{
+				BoardID: "00000000-0000-0000-0000-000000000000",
+				ColNo:   4,
+			},
+			wantErr: errColNoOutOfBounds,
+		},
+		{
+			name: "TitleEmpty",
+			req: PostReq{
+				BoardID: "00000000-0000-0000-0000-000000000000",
+				ColNo:   2,
+				Title:   "",
+			},
+			wantErr: errTitleEmpty,
+		},
+		{
+			name: "TitleTooLong",
+			req: PostReq{
+				BoardID: "00000000-0000-0000-0000-000000000000",
+				ColNo:   2,
+				Title:   "asdqweasdqweasdqweasdqweasdqweasdqweasdqweasdqweasd",
+			},
+			wantErr: errTitleTooLong,
+		},
+		{
+			name: "DescriptionTooLong",
+			req: PostReq{
+				BoardID: "00000000-0000-0000-0000-000000000000",
+				ColNo:   2,
+				Title:   "Some Task",
+				Description: "asdqweasdqweasdqweasdqweasdqweasdqweasdqweasdqw" +
+					"easdasdqweasdqweasdqweasdqweasdqweasdqweasdqweasdqweasda" +
+					"sdqweasdqweasdqweasdqweasdqweasdqweasdqweasdqweasdasdqwe" +
+					"asdqweasdqweasdqweasdqweasdqweasdqweasdqweasdasdqweasdqw" +
+					"easdqweasdqweasdqweasdqweasdqweasdqweasdasdqweasdqweasdq" +
+					"weasdqweasdqweasdqweasdqweasdqweasdasdqweasdqweasdqweasd" +
+					"qweasdqweasdqweasdqweasdqweasdasdqweasdqweasdqweasdqweas" +
+					"dqweasdqweasdqweasdqweasdasdqweasdqweasdqweasdqweasdqwea" +
+					"sdqweasdqweasdqweasdasdqweasdqweasdqweasdqweasdqweasdqwe" +
+					"asdqwe",
+			},
+			wantErr: errDescriptionTooLong,
+		},
+		{
+			name: "SubtaskTitleEmpty",
+			req: PostReq{
+				BoardID:     "00000000-0000-0000-0000-000000000000",
+				ColNo:       2,
+				Title:       "Some Task",
+				Description: "Some Description",
+				Subtasks:    []tasktbl.Subtask{{Title: ""}},
+			},
+			wantErr: errSubtaskTitleEmpty,
+		},
+		{
+			name: "SubtaskTitleTooLong",
+			req: PostReq{
+				BoardID:     "00000000-0000-0000-0000-000000000000",
+				ColNo:       2,
+				Title:       "Some Task",
+				Description: "Some Description",
+				Subtasks: []tasktbl.Subtask{
+					{
+						Title: "asdqweasdqweasdqweasdqweasdqweasdqweasdqweasd" +
+							"qweasd",
+					},
+				},
+			},
+			wantErr: errSubtaskTitleTooLong,
+		},
+		{
+			name: "OrderNegative",
+			req: PostReq{
+				BoardID:     "00000000-0000-0000-0000-000000000000",
+				ColNo:       2,
+				Title:       "Some Task",
+				Description: "Some Description",
+				Subtasks:    []tasktbl.Subtask{{Title: "Some Subtask"}},
+				Order:       -1,
+			},
+			wantErr: errOrderNegative,
+		},
+		{
+			name: "OK",
+			req: PostReq{
+				BoardID:     "00000000-0000-0000-0000-000000000000",
+				ColNo:       2,
+				Title:       "Some Task",
+				Description: "Some Description",
+				Subtasks:    []tasktbl.Subtask{{Title: "Some Subtask"}},
+				Order:       0,
+			},
+			wantErr: nil,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			err := sut(c.req)
+			assert.ErrIs(t.Error, err, c.wantErr)
+		})
+	}
+}
 
 // TestTitleValidator tests the TitleValidator.Validate method.
 func TestTitleValidator(t *testing.T) {
